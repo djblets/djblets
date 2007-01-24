1f6009cf8c5d4258f50368baa0897cc44692b8cd
chipx86 <chipx86@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-01-24 07:44:21 +0000

    Import newforms.
    git-svn-id: http://svn.navi.cx/misc/trunk/djblets@11261 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- djblets/auth/util.py
+++ djblets/auth/util.py
@@ -19,6 +19,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
+from django import newforms as forms
 from django.contrib import auth
 from django.conf import settings
 from django.http import HttpResponseRedirect
