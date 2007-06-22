535c296e841082bb0e86cd6c569c99edebcf8b61
chipx86 <chipx86@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-06-22 00:07:09 +0000

    Added a new decorator for simplifying the creation of templatetags that span over a block of nodes ({% foo %} ... {% endfoo %}). This does all the hard, redundant work of parsing the parameters, figuring out which are optional, setting up the class, and resolving variables. The implementation just has to render the content. This greatly simplifies our tags implementation. Reviewed at http://reviews.review-board.org/r/90
    git-svn-id: http://svn.navi.cx/misc/trunk/djblets@11595 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- djblets/util/decorators.py
+++ djblets/util/decorators.py
@@ -24,6 +24,12 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
+from inspect import getargspec
+
+from django import template
+from django.template import resolve_variable
+from django.template import TemplateSyntaxError, VariableDoesNotExist
+
 
 # The decorator decorator.  This is copyright unknown, verbatim from
 # http://wiki.python.org/moin/PythonDecoratorLibrary
@@ -50,3 +56,57 @@ def simple_decorator(decorator):
     new_decorator.__dict__.update(decorator.__dict__)
     return new_decorator
 
+
+def blocktag(tag_func):
+    """
+    A decorator similar to Django's @register.simple_tag that does all the
+    redundant work of parsing arguments and creating a node class in order
+    to render content between a foo and endfoo tag block. This condenses
+    many tag implementations down to a few lines of code.
+
+    Example:
+        @register.tag
+        @blocktag
+        def divify(context, nodelist, div_id=None):
+            s = "<div"
+            if div_id:
+                s += " id='%s'" % div_id
+            return s + ">" + nodelist.render(context) + "</div>"
+    """
+    class BlockTagNode(template.Node):
+        def __init__(self, tag_name, tag_func, nodelist, args):
+            self.tag_name = tag_name
+            self.tag_func = tag_func
+            self.nodelist = nodelist
+            self.args = args
+
+        def render(self, context):
+            args = [resolve_variable(var, context) for var in self.args]
+            return self.tag_func(context, self.nodelist, *args)
+
+    def _setup_tag(parser, token):
+        bits = token.split_contents()
+        tag_name = bits[0]
+        del(bits[0])
+
+        params, xx, xxx, defaults = getargspec(tag_func)
+        max_args = len(params) - 2 # Ignore context and nodelist
+        min_args = max_args - len(defaults or [])
+
+        if not min_args <= len(bits) <= max_args:
+            if min_args == max_args:
+                raise TemplateSyntaxError, \
+                    "%r tag takes %d arguments." % (tag_name, num_args)
+            else:
+                raise TemplateSyntaxError, \
+                    "%r tag takes %d to %d arguments, got %d." % \
+                    (tag_name, min_args, max_args, len(bits))
+
+        nodelist = parser.parse(('end%s' % tag_name),)
+        parser.delete_first_token()
+        return BlockTagNode(tag_name, tag_func, nodelist, bits)
+
+    _setup_tag.__name__ = tag_func.__name__
+    _setup_tag.__doc__ = tag_func.__doc__
+    _setup_tag.__dict__.update(tag_func.__dict__)
+    return _setup_tag
