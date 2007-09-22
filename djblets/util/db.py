dd4e21d7b5acf5abcd32f2f8dc1c1135adad1c3a
jupiter <jupiter@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-09-22 23:50:28 +0000

    Add QLeftOuterJoins from http://www.djangosnippets.org/snippets/274/
    git-svn-id: http://svn.navi.cx/misc/trunk/djblets@11651 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ djblets/util/db.py
@@ -0,0 +1,56 @@
+#
+# db.py -- Database utilities.
+#
+# Copyright (c) 2007  David Trowbridge
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+
+from django.db.models.query import Q, QAnd, QOr
+
+
+#
+# This is a hack to work around the stupid default behavior of Q objects and OR
+# operators.  By default, querying (Q(x) | Q(y)) | Q(z) when foreign keys are
+# involved will result in some small set of objects, instead of the union of x,
+# y and z.
+#
+# This implementation comes from http://www.djangosnippets.org/snippets/274/
+# By user "dottedmag" on djangosnippets.  Distributed under the generic terms of
+# the djangosnippets web site.
+#
+class QLeftOuterJoins(Q):
+    "Replaces all INNER JOINs with LEFT OUTER JOINs inside"
+    def __init__(self, q):
+        self.q = q
+
+    def __and__(self, other):
+        return QAnd(self, other)
+
+    def __or__(self, other):
+        return QOr(self, other)
+
+    def get_sql(self, opts):
+        joins, where, params = self.q.get_sql(opts)
+        for join_name, join in joins.iteritems():
+            joins[join_name] = (join[0], "LEFT OUTER JOIN", join[2])
+        return joins, where, params
+
