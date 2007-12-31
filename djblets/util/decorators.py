5bec6678ea0575029dcc4455990f36530b901f5e
chipx86 <chipx86@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2007-12-31 09:26:09 +0000

    Added a basictag decorator for creating template tags. It works like @register.simple_tag, but has an optional takes_context parameter for passing a context to the tag. This lets us remove a lot of the boilerplate in simple tags that need a context.
    git-svn-id: http://svn.navi.cx/misc/trunk/djblets@11670 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- djblets/util/decorators.py
+++ djblets/util/decorators.py
@@ -3,6 +3,7 @@
 #                  to something with a different name.
 #
 # Copyright (c) 2007  David Trowbridge
+# Copyright (c) 2007  Christian Hammond
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -28,7 +29,7 @@ from inspect import getargspec
 
 from django import template
 from django.template import resolve_variable
-from django.template import TemplateSyntaxError
+from django.template import TemplateSyntaxError, Variable
 
 
 # The decorator decorator.  This is copyright unknown, verbatim from
@@ -57,6 +58,71 @@ def simple_decorator(decorator):
     return new_decorator
 
 
+def basictag(takes_context=False):
+    """
+    A decorator similar to Django's @register.simple_tag that optionally
+    takes a context parameter. This condenses many tag implementations down
+    to a few lines of code.
+
+    Example:
+        @register.tag
+        @basictag(takes_context=True)
+        def printuser(context):
+            return context['user']
+    """
+    class BasicTagNode(template.Node):
+        def __init__(self, take_context, tag_name, tag_func, args):
+            self.takes_context = takes_context
+            self.tag_name = tag_name
+            self.tag_func = tag_func
+            self.args = args
+
+        def render(self, context):
+            args = [Variable(var).resolve(context) for var in self.args]
+
+            if self.takes_context:
+                return self.tag_func(context, *args)
+            else:
+                return self.tag_func(*args)
+
+    def basictag_func(tag_func):
+        def _setup_tag(parser, token):
+            bits = token.split_contents()
+            tag_name = bits[0]
+            del(bits[0])
+
+            params, xx, xxx, defaults = getargspec(tag_func)
+            max_args = len(params)
+
+            if takes_context:
+                if params[0] == 'context':
+                    max_args -= 1 # Ignore context
+                else:
+                    raise TemplateSyntaxError, \
+                        "Any tag function decorated with takes_context=True " \
+                        "must have a first argument of 'context'"
+
+            min_args = max_args - len(defaults or [])
+
+            if not min_args <= len(bits) <= max_args:
+                if min_args == max_args:
+                    raise TemplateSyntaxError, \
+                        "%r tag takes %d arguments." % (tag_name, min_args)
+                else:
+                    raise TemplateSyntaxError, \
+                        "%r tag takes %d to %d arguments, got %d." % \
+                        (tag_name, min_args, max_args, len(bits))
+
+            return BasicTagNode(takes_context, tag_name, tag_func, bits)
+
+        _setup_tag.__name__ = tag_func.__name__
+        _setup_tag.__doc__ = tag_func.__doc__
+        _setup_tag.__dict__.update(tag_func.__dict__)
+        return _setup_tag
+
+    return basictag_func
+
+
 def blocktag(tag_func):
     """
     A decorator similar to Django's @register.simple_tag that does all the
@@ -81,7 +147,7 @@ def blocktag(tag_func):
             self.args = args
 
         def render(self, context):
-            args = [resolve_variable(var, context) for var in self.args]
+            args = [Variable(var).resolve(context) for var in self.args]
             return self.tag_func(context, self.nodelist, *args)
 
     def _setup_tag(parser, token):
