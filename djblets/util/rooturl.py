810f285dd42ab8f247bf976d5ed50a731cf3a100
chipx86 <chipx86@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2008-04-17 08:15:49 +0000

    Import handler404 and handler500 so that 404 and 500 pages work.
    git-svn-id: http://svn.navi.cx/misc/trunk/djblets@11746 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- djblets/util/rooturl.py
+++ djblets/util/rooturl.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, handler404, handler500
 
 urlpatterns = patterns('',
     (r'^%s' % settings.SITE_ROOT[1:], include(settings.SITE_ROOT_URLCONF)),
