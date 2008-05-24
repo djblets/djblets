81d6c2fe4f7f5a2cfa59147b922fd1a68304354b
Christian Hammond <chipx86@chipx86.com>
2008-05-24 14:23:32 -0700

    Use named URLs for the enable/disable extension links.

--- djblets/extensions/urls.py
+++ djblets/extensions/urls.py
@@ -1,11 +1,15 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include, handler404, handler500
+from django.conf.urls.defaults import patterns, include, url, \
+                                      handler404, handler500
 
 
 urlpatterns = patterns('djblets.extensions.views',
     (r'^$', 'extension_list'),
-    (r'^(?P<ext_class>([A-Za-z0-9_.])+)/enable/$', 'set_extension_enabled',
-     {'enabled': True}),
-    (r'^(?P<ext_class>([A-Za-z0-9_.])+)/disable/$', 'set_extension_enabled',
-     {'enabled': False}),
+
+    url(r'^(?P<ext_class>[A-Za-z0-9_.]+)/enable/$', 'set_extension_enabled',
+        {'enabled': True},
+        name="enable_extension"),
+    url(r'^(?P<ext_class>[A-Za-z0-9_.]+)/disable/$', 'set_extension_enabled',
+        {'enabled': False},
+        name="disable_extension"),
 )
