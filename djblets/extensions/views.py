3229cf1b7debb418a5f2c711e491a2b12369ca96
Christian Hammond <chipx86@chipx86.com>
2008-06-11 15:29:56 -0700

    Add a configure_extension view to help with basic extension configuration.

--- djblets/extensions/views.py
+++ djblets/extensions/views.py
@@ -22,3 +22,30 @@ def set_extension_enabled(request, ext_class, enabled, extension_manager):
         extension_manager.disable_extension(ext_class)
 
     return HttpResponseRedirect("../../")
+
+
+@staff_member_required
+def configure_extension(request, ext_class, form_class, extension_manager,
+                        template_name='extensions/configure_extension.html'):
+    context = {}
+    form = None
+    extension = extension_manager.get_enabled_extension(ext_class.id)
+
+    if extension:
+        context['extension'] = extension
+        context['enabled'] = True
+
+        if extension.is_configurable:
+            if request.method == 'POST':
+                form = form_class(extension, request.POST, request.FILES)
+
+                if form.is_valid():
+                    form.save()
+            else:
+                form = form_class(extension)
+
+            context['form'] = form
+    else:
+        context['enabled'] = False
+
+    return render_to_response(template_name, RequestContext(request, context))
