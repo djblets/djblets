7060aabaf4132a5a3b159927945249ed1b6b3b3b
Christian Hammond <chipx86@chipx86.com>
2008-07-07 02:14:39 -0700

    Add forms and templates for doing settings.

---
+++ djblets/extensions/forms.py
@@ -0,0 +1,23 @@
+from django import newforms as forms
+
+
+class SettingsForm(forms.Form):
+    """
+    A base form for loading/saving settings for an extension. This is meant
+    to be overridden by extensions to provide configuration pages. Any fields
+    defined by the form will be loaded and saved automatically.
+    """
+    def __init__(self, extension, *args, **kwargs):
+        forms.Form.__init__(self, *args, **kwargs)
+        self.extension = extension
+
+        for field in self.fields:
+            if field in self.extension.settings:
+                self.fields[field].initial = self.extension.settings[field]
+
+    def save(self):
+        if not self.errors:
+            for key, value in self.cleaned_data.iteritems():
+                self.extension.settings[key] = value
+
+            self.extension.settings.save()
