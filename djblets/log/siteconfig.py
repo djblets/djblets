9ed0723c8a10b9865f36c9b3b971987198e19d30
chipx86 <chipx86@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2008-09-05 10:47:53 +0000

    Add the siteconfig.py file for logging.
    git-svn-id: http://svn.navi.cx/misc/trunk/djblets@11866 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ djblets/log/siteconfig.py
@@ -0,0 +1,11 @@
+settings_map = {
+    'logging_enabled':         'LOGGING_ENABLED',
+    'logging_directory':       'LOGGING_DIRECTORY',
+    'logging_allow_profiling': 'LOGGING_ALLOW_PROFILING',
+}
+
+defaults = {
+    'logging_enabled':         False,
+    'logging_directory':       None,
+    'logging_allow_profiling': False,
+}
