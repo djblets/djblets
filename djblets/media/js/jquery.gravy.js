aa0a175976e239f3536f4eca0b1d975add520418
chipx86 <chipx86@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2008-11-19 08:56:15 +0000

    This adds a funcQueue utility function to djblets.gravy. The purpose of this is to queue up several functions, allowing them to operate one at a time. It is up to the functions to invoke the funcQueue.next() function to invoke the next function in the list. This is useful for chaining ajax calls so only one operates at a time.  It is intentionally very simple. Reviewed at http://reviews.review-board.org/r/635/
    git-svn-id: http://svn.navi.cx/misc/trunk/djblets@11907 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- djblets/media/js/jquery.gravy.js
+++ djblets/media/js/jquery.gravy.js
@@ -1,3 +1,5 @@
+(function($) {
+
 jQuery.fn.extend({
     /*
      * Makes an element unselectable.
@@ -52,3 +54,54 @@ jQuery.extend(String, {
         return str;
     }
 });
+
+var queues = {};
+
+/*
+ * A set of utility functions for implementing a queue of functions.
+ * Functions are added to the queue and, when their operation is complete,
+ * they are to call next(), which will trigger the next function in the
+ * queue.
+ *
+ * There can be multiple simultaneous queues going at once. They're identified
+ * by queue names that are passed to funcQueue().
+ */
+$.funcQueue = function(name) {
+    var self = this;
+
+    if (!queues[name]) {
+        queues[name] = [];
+    }
+
+    /*
+     * Adds a function to the queue.
+     *
+     * @param {function} func  The function to add.
+     */
+    this.add = function(func) {
+        queues[name].push(func);
+    };
+
+    /*
+     * Invokes the next function in the queue.
+     */
+    this.next = function() {
+        if (queues[name].length == 0) {
+            return;
+        }
+
+        var func = queues[name].shift();
+        func();
+    };
+
+    /*
+     * Begins the queue.
+     */
+    this.start = function() {
+        self.next();
+    };
+
+    return this;
+}
+
+})(jQuery);
