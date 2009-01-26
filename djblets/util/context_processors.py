07d80f39277a4c9ab6f238ded5f74fe5e0c79dce
chipx86 <chipx86@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-01-26 07:16:33 +0000

    This adds the ability to scan all installed media files (or a subset of them) to find the last modified file's timestamp. This is used as a media serial suffix, appended to filenames as a query argument (for example, foo.png?20081228). This can be used to add persistent browser-side caching for media files. Each of these files can be set with an Expires of 2 years, or some other large amount of time, without the worry of files going stale after being updated.
    There's also support for an AJAX serial number, so that we can cache
    ajax loads yet set a new serial to load when we make a change down the
    road.
    
    Reviewed at http://reviews.review-board.org/r/683/
    
    
    git-svn-id: http://svn.navi.cx/misc/trunk/djblets@11948 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- djblets/util/context_processors.py
+++ djblets/util/context_processors.py
@@ -1,7 +1,8 @@
 #
 # djblets/util/context_processors.py
 #
-# Copyright (c) 2007  David Trowbridge
+# Copyright (c) 2007-2009  Christian Hammond
+# Copyright (c) 2007-2009  David Trowbridge
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -23,6 +24,9 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
+import os
+from datetime import datetime
+
 from django.conf import settings
 
 def settingsVars(request):
@@ -35,3 +39,67 @@ def siteRoot(request):
     proper support for basing the installation in a subdirectory.
     """
     return {'SITE_ROOT': settings.SITE_ROOT}
+
+
+MEDIA_SERIAL = 0
+
+def mediaSerial(request):
+    """
+    Exposes a media serial number that can be appended to a media filename
+    in order to make a URL that can be cached forever without fear of change.
+    The next time the file is updated and the server is restarted, a new
+    path will be accessed and cached.
+
+    If settings.MEDIA_SERIAL exists, this will use that. Otherwise, it will
+    crawl the media files (using directories in MEDIA_SERIAL_DIRS if
+    specified, or all of MEDIA_ROOT otherwise), figuring out the latest
+    timestamp, and use that instead.
+    """
+    global MEDIA_SERIAL
+
+    if hasattr(settings, "MEDIA_SERIAL"):
+        MEDIA_SERIAL = settings.MEDIA_SERIAL
+    elif MEDIA_SERIAL == 0:
+        media_dirs = getattr(settings, "MEDIA_SERIAL_DIRS", ["."])
+
+        for media_dir in media_dirs:
+            media_path = os.path.join(settings.MEDIA_ROOT, media_dir)
+
+            for root, dirs, files in os.walk(media_path):
+                for name in files:
+                    mtime = int(os.stat(os.path.join(root, name)).st_mtime)
+
+                    if mtime > MEDIA_SERIAL:
+                        MEDIA_SERIAL = mtime
+
+    return {'MEDIA_SERIAL': MEDIA_SERIAL}
+
+
+AJAX_SERIAL = 0
+
+def ajaxSerial(request):
+    """
+    Exposes a serial number that can be appended to filenames involving
+    dynamic loads of URLs in order to make a URL that can be cached forever
+    without fear of change.
+
+    If settings.AJAX_SERIAL exists, this will use that. Otherwise, it will
+    crawl the template files (using directories in TEMPLATE_DIRS), figuring
+    out the latest timestamp, and use that instead.
+    """
+    global AJAX_SERIAL
+
+    if hasattr(settings, "AJAX_SERIAL"):
+        AJAX_SERIAL = settings.AJAX_SERIAL
+    elif AJAX_SERIAL == 0:
+        template_dirs = getattr(settings, "TEMPLATE_DIRS", ["."])
+
+        for template_path in template_dirs:
+            for root, dirs, files in os.walk(template_path):
+                for name in files:
+                    mtime = int(os.stat(os.path.join(root, name)).st_mtime)
+
+                    if mtime > AJAX_SERIAL:
+                        AJAX_SERIAL = mtime
+
+    return {'AJAX_SERIAL': AJAX_SERIAL}
