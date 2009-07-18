8dceb6548fbad7630c348cdd32ba6270d8022801
chipx86 <chipx86@9a66fdd2-3ab9-0310-9df6-91dd0304fdbc>
2009-07-18 21:48:42 +0000

    Add djblets.util.filesystem.is_exe_in_path, which will check if the specified executable is in the executable search path.
    This function will do intelligent file extension determination. It expects
    an application name without a file extension (such as "cvs") and will append
    the ".exe" on Windows. This is an improvement over the method we used in
    Review Board before where we blindly searched for "cvs" (which could in theory
    exist yet not be an executable) on Windows, or "cvs.exe" (which could in
    theory exist yet not be an executable) on Linux.
    
    Reviewed at http://reviews.review-board.org/r/917/
    
    
    git-svn-id: http://svn.navi.cx/misc/trunk/djblets@12153 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- djblets/util/misc.py
+++ djblets/util/misc.py
@@ -1,7 +1,8 @@
 #
 # misc.py -- Miscellaneous utilities.
 #
-# Copyright (c) 2007  David Trowbridge
+# Copyright (c) 2007-2009  Christian Hammond
+# Copyright (c) 2007-2009  David Trowbridge
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
