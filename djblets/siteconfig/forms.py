b0607176c1069dfbcc4f91ef53d9685ba7f30af9
David Trowbridge <trowbrds@gmail.com>
2009-08-22 14:56:13 -0700

    Add copyright and MIT license headers to everything that's not empty.

--- djblets/siteconfig/forms.py
+++ djblets/siteconfig/forms.py
@@ -1,7 +1,7 @@
 #
-# djblets/siteconfig/forms.py
+# forms.py -- Forms for the siteconfig app.
 #
-# Copyright (c) 2008  Christian Hammond
+# Copyright (c) 2008-2009  Christian Hammond
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
--- djblets/util/__init__.py
+++ djblets/util/__init__.py
@@ -1,7 +1,7 @@
 #
-# __init__.py - djblets.util top-level
+# __init__.py -- djblets.util top-level
 #
-# Copyright (c) 2007  David Trowbridge
+# Copyright (c) 2007-2009  David Trowbridge
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
