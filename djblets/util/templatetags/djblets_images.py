ad122f2497cf516e80863a3ad8e8c306821f4936
David Trowbridge <trowbrds@gmail.com>
2009-09-16 12:15:31 -0700

    Rewrite djblets_images tags to use storage backends.
    When I wrote these utilities (cropping and thumbnailing), I didn't take into
    account that Django provides a storage abstraction. The default storage is on
    the local filesystem, but there are third-party backends that also work. This
    change updates them to use the storage APIs instead of working directly with
    file objects on the local disk.
    
    This change is in preparation for a corresponding review-board change to allow
    the use of django-storages backends (specifically Amazon S3) for screenshot
    storage.
    
    Testing done: Tested screenshot thumbnailing against screenshots stored in
    Amazon S3
    
    Reviewed at http://reviews.review-board.org/r/1078/

--- djblets/util/templatetags/djblets_images.py
+++ djblets/util/templatetags/djblets_images.py
@@ -27,6 +27,11 @@
 import os
 
 try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+try:
     from PIL import Image
 except ImportError:
     import Image
@@ -45,6 +50,7 @@ def crop_image(file, x, y, width, height):
     resulting URL of the cropped image.
     """
     filename = file.name
+    storage = file.storage
 
     if filename.find(".") != -1:
         basename, format = filename.rsplit('.', 1)
@@ -53,18 +59,22 @@ def crop_image(file, x, y, width, height):
         basename = filename
         new_name = '%s_%s_%s_%s_%s' % (basename, x, y, width, height)
 
-    new_path = os.path.join(settings.MEDIA_ROOT, new_name)
-    new_url = os.path.join(settings.MEDIA_URL, new_name)
-
-    if not os.path.exists(new_path):
+    if not storage.exists(new_name):
         try:
-            image = Image.open(os.path.join(settings.MEDIA_ROOT, filename))
+            file = storage.open(filename)
+            data = StringIO(file.read())
+            file.close()
+
+            image = Image.open(data)
             image = image.crop((x, y, x + width, y + height))
-            image.save(new_path, image.format)
+
+            new_file = storage.open(new_name, 'wb')
+            image.save(new_file, image.format)
+            new_file.close()
         except (IOError, KeyError):
             return ""
 
-    return new_url
+    return storage.url(new_name)
 
 
 # From http://www.djangosnippets.org/snippets/192
@@ -84,17 +94,21 @@ def thumbnail(file, size='400x100'):
         basename = filename
         miniature = '%s_%s' % (basename, size)
 
-    miniature_filename = os.path.join(settings.MEDIA_ROOT, miniature)
-    miniature_url = os.path.join(settings.MEDIA_URL, miniature)
+    storage = file.storage
 
-    if not os.path.exists(miniature_filename):
+    if not storage.exists(miniature):
         try:
-            image = Image.open(os.path.join(settings.MEDIA_ROOT, filename))
+            file = storage.open(filename, 'rb')
+            data = StringIO(file.read())
+            file.close()
+
+            image = Image.open(data)
             image.thumbnail([x, y], Image.ANTIALIAS)
-            image.save(miniature_filename, image.format)
-        except IOError:
-            return ""
-        except KeyError:
+
+            new_file = storage.open(miniature, 'wb')
+            image.save(new_file, image.format)
+            new_file.close()
+        except (IOError, KeyError), e:
             return ""
 
-    return miniature_url
+    return storage.url(miniature)
