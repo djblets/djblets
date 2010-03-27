d96fe64288c811b857107a8454e66508b2270eeb
Christian Hammond <chipx86@chipx86.com>
2010-03-27 16:34:35 -0700

    Bump to 0.5.9.dev.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -31,7 +31,7 @@
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 5, 8, 'final', 0, True)
+VERSION = (0, 5, 9, 'alpha', 0, False)
 
 
 def get_version_string():
