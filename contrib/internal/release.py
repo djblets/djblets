98b8bc6b82f0d4c698057588ed08aa34cef8523e
Christian Hammond <chipx86@chipx86.com>
2010-09-14 04:21:53 -0700

    Release Djblets 0.6.4.

--- contrib/internal/release.py
+++ contrib/internal/release.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
 from djblets import __version__, __version_info__, is_release
 
 
-PY_VERSIONS = ["2.4", "2.5", "2.6"]
+PY_VERSIONS = ["2.4", "2.5", "2.6", "2.7"]
 
 LATEST_PY_VERSION = PY_VERSIONS[-1]
 
