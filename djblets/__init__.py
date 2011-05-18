8e0622a72d0705cf6a4a6d9ce0cd17015c3ab368
David Trowbridge <trowbrds@gmail.com>
2011-05-17 23:43:59 -0700

    Mark as alpha instead of final.
    Build release builds as 0.6.8.alpha instead of 0.6.8.final.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -31,7 +31,7 @@
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 6, 8, 'final', 0, False)
+VERSION = (0, 6, 8, 'alpha', 0, False)
 
 
 def get_version_string():
