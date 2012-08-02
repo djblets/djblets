5973559890d6610e79f7919ff6590d990d0358c8
Christian Hammond <chipx86@chipx86.com>
2012-08-02 02:19:30 -0700

    Require Django 1.3.3.
    Now that Djblets 0.7 is out, and much of our code depends on modern
    Django releases, we now require Django 1.3.3 specifically. This is to
    ensure compatibility without any quirks from a newer Django.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(name=PACKAGE_NAME,
       license="MIT",
       description="A collection of useful classes and functions for Django",
       packages=find_packages(),
-      install_requires=['Django>=1.1.1', 'PIL'],
+      install_requires=['Django==1.3.3', 'PIL'],
       dependency_links = [
           "http://downloads.reviewboard.org/mirror/",
           download_url,
