dadd1d8599a79895950e3d29f40b2fba4ced7439
Christian Hammond <chipx86@chipx86.com>
2013-02-21 00:58:38 -0800

    Bump to 0.7.12 dev.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -31,7 +31,7 @@
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 7, 11, 'final', 0, True)
+VERSION = (0, 7, 12, 'alpha', 0, False)
 
 
 def get_version_string():
