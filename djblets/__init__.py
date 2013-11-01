0e95381ed4121af36a96ddc7431d9b42f9dc7ba0
Christian Hammond <chipx86@chipx86.com>
2013-11-01 16:43:07 -0700

    Release Djblets 0.7.22.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -31,7 +31,7 @@
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 7, 21, 'final', 0, True)
+VERSION = (0, 7, 22, 'final', 0, True)
 
 
 def get_version_string():
