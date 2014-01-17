055f796868d2ca9f66e19535ee15ff4b8dcd483e
Christian Hammond <chipx86@chipx86.com>
2014-01-16 16:29:26 -0800

    Bump to 0.8 alpha 3 dev.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 8, 0, 'alpha', 2, True)
+VERSION = (0, 8, 0, 'alpha', 3, False)
 
 
 def get_version_string():
