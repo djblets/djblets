8b501f3143cb6cf7815be9bcd54dd7d4802628dc
Christian Hammond <chipx86@chipx86.com>
2014-02-20 17:20:09 -0800

    Release Djblets 0.8 beta 1.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 8, 0, 'alpha', 3, False)
+VERSION = (0, 8, 0, 'beta', 1, True)
 
 
 def get_version_string():
