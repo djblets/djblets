0638d1fbba230bf69a8dbe92f5ffd3a21194d0d1
Christian Hammond <chipx86@chipx86.com>
2014-02-20 17:23:13 -0800

    Bump to 0.8 beta 2 dev.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 8, 0, 'beta', 1, True)
+VERSION = (0, 8, 0, 'beta', 1, False)
 
 
 def get_version_string():
