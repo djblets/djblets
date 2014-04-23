0c35441c538336ba31a16a9ed6e1c51796f13701
Georgy Dyuldin <g.dyuldin@gmail.com>
2014-04-22 17:22:52 -0700

    Fixed get_url_params_except function to work with not ascii contain query
    There are an error in urlencode if query contains non ascii strings.
    
    Reviewed at https://reviews.reviewboard.org/r/5722/

--- djblets/util/http.py
+++ djblets/util/http.py
@@ -206,7 +206,7 @@ def get_url_params_except(query, *params):
     others.
     """
     return urlencode([
-        (key, value)
+        (key, value.encode('utf-8'))
         for key, value in six.iteritems(query)
         if key not in params
     ])
