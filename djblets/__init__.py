500480a6234f5e8351e8230b7ee1d781344dad95
Christian Hammond <chipx86@chipx86.com>
2014-05-19 17:03:18 -0700

    Release Djblets 0.8.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 8, 0, 'rc', 3, False)
+VERSION = (0, 8, 0, 'final', 0, True)
 
 
 # Required version of Django
