394a6e57a75212e6399af0a709c6e7e7f2962e45
Christian Hammond <christian@beanbaginc.com>
2014-09-24 00:28:38 -0700

    Bump to 0.8.12 dev.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 8, 11, 'final', 0, True)
+VERSION = (0, 8, 12, 'alpha', 0, False)
 
 
 # Required version of Django
