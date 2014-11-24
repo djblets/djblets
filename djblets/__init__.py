9606e574df789f806aa3c9b4840626156efd0c6a
Christian Hammond <christian@beanbaginc.com>
2014-11-24 01:16:11 -0800

    Release Djblets 0.8.13.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 8, 12, 'final', 0, True)
+VERSION = (0, 8, 13, 'final', 0, True)
 
 
 # Required version of Django
