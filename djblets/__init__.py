c9a4a4cdf70f6416fb740de881b492cf7ffda1f2
Christian Hammond <christian@beanbaginc.com>
2015-03-09 18:27:22 -0700

    Release Djblets 0.8.16.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,11 +33,11 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 8, 16, 'alpha', 0, False)
+VERSION = (0, 8, 16, 'final', 0, True)
 
 
 # Required version of Django
-django_version = 'Django>=1.6.9,<1.7'
+django_version = 'Django>=1.6.10,<1.7'
 
 
 def get_version_string():
