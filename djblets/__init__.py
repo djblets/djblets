228f1795d24aedd1bc01eddbcefde12c55ea579b
Christian Hammond <christian@beanbaginc.com>
2015-03-17 18:47:48 -0700

    Bump to 0.9 beta 2 dev.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 9, 0, 'beta', 1, True)
+VERSION = (0, 9, 0, 'beta', 2, False)
 
 
 # Required version of Django
