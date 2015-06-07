282959e94b1cde1e1eaf5556128a99c71a2d6652
Christian Hammond <christian@beanbaginc.com>
2015-06-06 19:27:37 -0700

    Add documentation for djblets.configforms.
    This improves upon the documentation we already had to cover the
    capabilities of the various configform components. It also fleshes out
    the return values and arguments for the different functions.
    
    Testing Done:
    Built the docs and read through them.
    
    Reviewed at https://reviews.reviewboard.org/r/7389/

--- djblets/configforms/pages.py
+++ djblets/configforms/pages.py
@@ -1,3 +1,5 @@
+"""Base support for configuration pages."""
+
 from __future__ import unicode_literals
 
 from django.template.context import RequestContext
@@ -8,15 +10,40 @@ class ConfigPage(object):
     """Base class for a page of configuration forms.
 
     Each ConfigPage is represented in the main page by an entry in the
-    navigation sidebar. When the user has navigated to that page, any
-    forms owned by the ConfigPage will be displayed.
+    navigation sidebar. When the user has navigated to that page, all visible
+    :py:class:`djblets.configforms.forms.ConfigPageForm` subclasses owned by
+    the ConfigPage will be displayed.
     """
+
+    #: The unique ID of the page.
+    #:
+    #: This must be unique across all ConfigPages at a given URL.
     page_id = None
+
+    #: The displayed title for the page.
+    #:
+    #: This will show up in the navigation sidebar.
     page_title = None
+
+    #: The list of form subclasses to display on the page.
     form_classes = None
+
+    #: The template used to render the page.
     template_name = 'configforms/config_page.html'
 
     def __init__(self, config_view, request, user):
+        """Initialize the page.
+
+        Args:
+            config_view (ConfigPagesView):
+                The view that manages this ConfigPage.
+
+            request (HttpRequest):
+                The HTTP request from the client.
+
+            user (User):
+                The user who is viewing the page.
+        """
         self.config_view = config_view
         self.request = request
         self.forms = [
@@ -25,12 +52,16 @@ class ConfigPage(object):
         ]
 
     def is_visible(self):
-        """Returns whether the page should be visible.
+        """Return whether the page should be visible.
 
         Visible pages are shown in the sidebar and can be navigated to.
 
         By default, a page is visible if at least one of its forms are
         also visible.
+
+        Returns:
+            bool:
+                ``True`` if the page will be rendered, or ``False`` otherwise.
         """
         for form in self.forms:
             if form.is_visible():
@@ -39,7 +70,18 @@ class ConfigPage(object):
         return False
 
     def render(self):
-        """Renders the page as HTML."""
+        """Render the page to a string.
+
+        :py:attr:`template_name` will be used to render the page. The
+        template will be passed ``page`` (this page's instance) and
+        ``forms`` (the list of :py:class:`ConfigPageForm` instances to
+        render).
+
+        Subclasses can override this to provide additional rendering logic.
+
+        Returns:
+            unicode: The rendered page as HTML.
+        """
         return render_to_string(
             self.template_name,
             RequestContext(self.request, {
