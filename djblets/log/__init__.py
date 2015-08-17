2999d55c1ac9abec25dc4502360ab0e5d5cf3947
Christian Hammond <christian@beanbaginc.com>
2015-08-17 14:08:38 -0700

    Split the logging settings docs into module-level docs.
    The LoggingMiddleware docs had a number of settings that weren't related
    to the middleware, and including them in the class docstring broke
    Sphinx's autodoc support. This has been moved into the djblets.log
    module documentation.
    
    Testing Done:
    Built the docs and looked through it.
    
    Reviewed at https://reviews.reviewboard.org/r/7596/

--- djblets/log/__init__.py
+++ djblets/log/__init__.py
@@ -1,28 +1,84 @@
-#
-# __init__.py -- Basic utilities for the log app
-#
-# Copyright (c) 2008-2009  Christian Hammond
-# Copyright (c) 2008-2009  David Trowbridge
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#
+"""Logging support.
+
+This can be used to offer some advanced logging capabilities.
+
+Note that this predates Django's modern logging support, and is here primarily
+for compatibility.
+
+
+Settings
+========
+
+The following settings control logging.
+
+
+LOGGING_ENABLED
+---------------
+
+Default: ``False``
+
+Sets whether or not logging is enabled.
+
+
+LOGGING_DIRECTORY
+-----------------
+
+Default: ``None``
+
+Specifies the directory that log files should be stored in.
+This directory must be writable by the process running Django.
+
+
+LOGGING_NAME
+------------
+
+Default: ``None``
+
+The name of the log files, excluding the extension and path. This will
+usually be the name of the website or web application. The file extension
+will be automatically appended when the file is written.
+
+
+LOGGING_ALLOW_PROFILING
+-----------------------
+
+Default: ``False``
+
+Specifies whether or not code profiling is allowed. If True, visiting
+any page with a ``?profiling=1`` parameter in the URL will cause the
+request to be profiled and stored in a ``.prof`` file using the defined
+``LOGGING_DIRECTORY`` and ``LOGGING_NAME``.
+
+
+LOGGING_LINE_FORMAT
+-------------------
+
+Default: ``"%(asctime)s - %(levelname)s - %(message)s"``
+
+The format for lines in the log file. See Python's :py:mod:`logging`
+documentation for possible values in the format string.
+
+
+LOGGING_PAGE_TIMES
+------------------
+
+Default: ``False``
+
+If enabled, page access times will be logged. Specifically, it will log
+the initial request, the finished render and response, and the total
+time it look.
+
+The username and page URL will be included in the logs.
+
+
+LOGGING_LEVEL
+-------------
+
+Default: ``"DEBUG"``
+
+The minimum level to log. Possible values are ``"DEBUG"``, ``"INFO"``,
+``"WARNING"``, ``"ERROR"`` and ``"CRITICAL"``.
+"""
 
 from __future__ import unicode_literals
 
