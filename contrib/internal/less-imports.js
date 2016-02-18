829a19cc6e6af26f3317680ea1d3b70cdcb4de01
David Trowbridge <trowbrds@gmail.com>
2016-02-17 16:18:34 -0800

    Add custom LessCSS compiler.
    The built-in Less compiler works fairly well, but has a couple issues that were
    annoying me.
    
    First of all, in order to cope with an old bug in lessc that no longer exists,
    it redirected the output of lessc rather than passed the output filename on the
    command line. This made it impossible to use the `--source-map` argument, which
    makes devtools a much nicer experience. I've reimplemented `compile_file` to
    pass in the outfile name on the command-line, and raised the question with the
    django-pipeline maintainer to see if we can't get that upstream.
    
    Second, the compiler was completely ignoring the outdated check and always
    recompiling every file on every load. This made sense because there was no
    dependency analysis, so if an `@import`ed file was changed, the dependent file
    would not be recompiled. This was slowing down reloads a fair bit (but it was
    still faster than recompiling in the browser).
    
    To fix this second part, I've written a helper, `less-imports.js`, which uses
    the less module's parser to get a deep list of the file imports. These are then
    each compared against the target output file's timestamp to see if anything is
    out of date. While this is still some overhead (since it still has to parse
    the files), it's quite a bit faster than actually recompiling.
    
    Testing done:
    - Loaded and reloaded things a bunch.
    - Made a change to defs.less and saw in the log that it was recompiling all the
      files that imported it.
    
    Reviewed at https://reviews.reviewboard.org/r/7982/

---
+++ contrib/internal/less-imports.js
@@ -0,0 +1,146 @@
+#!/usr/bin/env node
+
+/**
+ * Extract a list of import dependencies from a top-level .less file.
+ *
+ * This is used to determine whether or not any given .less file needs to be
+ * recompiled during development. The pipeline compiler in
+ * djblets.pipeline.compilers.less will run this script to extract a deep list
+ * of imports from a given file, which are then used to check the timestamps to
+ * see if any of the dependencies are newer than the target file.
+ *
+ * On success, this will print out the file paths to any import dependencies,
+ * one file per line, and exit with a return code of 0. On error, an error will
+ * be written to stderr and the process will exit with a non-zero return code.
+ */
+
+var fs = require('fs'),
+    less = require('less'),
+    path = require('path');
+
+/**
+ * Parse arguments.
+ *
+ * This will parse ``process.argv`` similar to how the lessc command does. In
+ * this case, we want to recognize the arguments that could be passed to lessc
+ * but we don't actually care about most of them since the only things that
+ * matter are those that affect import paths.
+ *
+ * Returns:
+ *     object:
+ *     An object with ``options`` and ``paths`` keys, containing the LessCSS
+ *     options (which should be passed into :js:func:`less.parse`) and the file
+ *     path of the .less file to scan.
+ */
+function parseArgs() {
+    var args = process.argv.slice(2),
+        options = {
+            color: false,
+            compless: false,
+            depends: false,
+            globalVars: null,
+            ieCompat: true,
+            insecure: false,
+            lint: false,
+            max_line_len: -1,
+            modifyVars: null,
+            paths: [],
+            plugins: [],
+            relativeUrls: false,
+            rootpath: '',
+            strictImports: false,
+            strictMath: false,
+            strictUnits: false,
+            urlArgs: ''
+        },
+        paths;
+
+    function checkArgFunc(arg, option) {
+        if (!option) {
+            console.error(arg + ' option requires a parameter');
+            process.exit(1);
+        }
+    }
+
+    function parseVariableOption(option, variables) {
+        var parts = option.split('=', 2);
+        variables[parts[0]] = parts[1];
+    }
+
+    paths = args.filter(function(arg) {
+        var match = arg.match(/^--?([a-z][0-9a-z-]*)(?:=(.*))?$/i);
+
+        if (match) {
+            arg = match[1];
+        } else {
+            return arg;
+        }
+
+        switch(arg) {
+            case 'global-var':
+                checkArgFunc(arg, match[2]);
+                if (!options.globalVars) {
+                    options.globalVars = {};
+                }
+                parseVariableOption(match[2], options.globalVars);
+                break;
+
+            case 'include-path':
+                checkArgFunc(arg, match[2]);
+                options.paths = match[2]
+                    .split(':')
+                    .map(function(p) {
+                        return path.resolve(process.cwd(), p);
+                    });
+                break;
+
+            case 'no-color':
+            case 'source-map':
+            case 'autoprefix':
+                // These don't matter for the imports check. No-op.
+                break;
+
+            default:
+                console.error('less-imports.js: Un-handled argument "%s"', arg);
+                process.exit(1);
+        }
+    });
+
+    return {
+        options: options,
+        paths: paths
+    };
+}
+
+var opts = parseArgs();
+if (opts.paths.length !== 1) {
+    console.error('Requires a single path argument');
+    process.exit(1);
+}
+
+var path = opts.paths[0];
+opts.options.filename = path;
+
+fs.readFile(path, function(err, data) {
+    if (err) {
+        console.error('Could not read file "%s": %s', path, err);
+        process.exit(1);
+    }
+
+    less.parse(data.toString('utf8'), opts.options, function(err, tree, imports) {
+        var files, i;
+
+        if (err) {
+            console.error('Could not parse file "%s": %s', path, err);
+            process.exit(1);
+        }
+
+        files = Object.keys(imports.files);
+
+        for (i = 0; i < files.length; i++) {
+            console.log(files[i]);
+        }
+
+        process.exit(0);
+    });
+});
---
+++ djblets/pipeline/compilers/less.py
@@ -0,0 +1,96 @@
+"""A specialization of pipeline's LessCompiler."""
+
+from __future__ import unicode_literals
+
+import logging
+import os.path
+from shutil import rmtree
+from tempfile import mkdtemp
+
+from pipeline.compilers.less import LessCompiler as PipelineLessCompiler
+from pipeline.conf import settings as pipeline_settings
+
+
+class LessCompiler(PipelineLessCompiler):
+    """A LESS compiler which doesn't redirect the output.
+
+    Pipeline's LESS compiler outputs to stdout and redirects that output to the
+    target file, because of some bug in old versions of lessc. We don't want to
+    do that, because it interferes with the generation of sourcemaps.
+
+    To use this, add the path to this class to
+    ``settings.PIPELINE['COMPILERS']``.
+    """
+
+    def compile_file(self, infile, outfile, outdated=False, force=False):
+        """Compile a given file.
+
+        Args:
+            infile (unicode):
+                The input filename.
+
+            outfile (unicode):
+                The output filename.
+
+            outdated (bool):
+                Whether the file is outdated.
+
+            force (bool):
+                Whether the compilation should proceed, regardless of whether
+                the file is out of date.
+        """
+        if outdated or force:
+            self.execute_command((
+                pipeline_settings.LESS_BINARY,
+                pipeline_settings.LESS_ARGUMENTS,
+                infile,
+                outfile
+            ))
+
+    def is_outdated(self, infile, outfile):
+        """Check to see if the given file is outdated.
+
+        This will compare the timestamps of the ``infile`` and ``outfile``, but
+        also compare the timestamp of ``outfile`` against any import
+        depedencies that exist.
+
+        Args:
+            infile (unicode):
+                The path to the input file.
+
+            outfile (unicode):
+                The path to the output file.
+
+        Returns:
+            bool:
+            True if the file is out of date and needs to be recompiled.
+        """
+        # If the file itself is outdated, just use that.
+        if super(LessCompiler, self).is_outdated(infile, outfile):
+            return True
+
+        # Now parse out a deep list of the imported files and check those.
+        tempdir = mkdtemp()
+        command = (
+            os.path.join(os.path.dirname(__file__), '..', '..', '..',
+                         'contrib', 'internal', 'less-imports.js'),
+            pipeline_settings.LESS_ARGUMENTS,
+            infile
+        )
+
+        imports_file = os.path.join(tempdir, os.path.basename(outfile))
+        self.execute_command(command, stdout_captured=imports_file)
+
+        with open(imports_file) as f:
+            files = f.read().splitlines()
+
+            for dep in files:
+                if super(LessCompiler, self).is_outdated(dep, outfile):
+                    logging.info('Recompiling %s because dependency %s is out '
+                                 'of date',
+                                 infile, dep)
+                    return True
+
+        rmtree(tempdir)
+
+        return False
