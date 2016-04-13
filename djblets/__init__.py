24d8e293e51fa1d48b15993e65b7678e6a1ecba6
Christian Hammond <christian@beanbaginc.com>
2016-04-13 14:49:26 -0700

    Bump to 0.9.4 dev.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 9, 3, 'final', 0, True)
+VERSION = (0, 9, 4, 'alpha', 0, False)
 
 
 #: The major version of Django we're using.
