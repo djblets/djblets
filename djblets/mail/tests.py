9554810ffbb3ab0e4b03e11a0cca383e68665204
Christian Hammond <christian@beanbaginc.com>
2016-09-18 16:12:53 -0700

    Add support for looking up DMARC records for a domain.
    DMARC is a mechanism used by mail servers and other mail software to
    decide what to do with e-mails if the sender fails a verification test
    (such as if an e-mail address in the From header is spoofed). Many
    companies set this to quarantine (e.g., mark as spam) or outright reject
    e-mail. Since many of our projects attempt to e-mail on behalf of users
    when they interact with our services, this leads to a very bad
    experience.
    
    This new support will later allow our e-mail code to be smarter so that
    it can decide whether to spoof or not based on the sender domain's DMARC
    settings.
    
    There's one public function available for use: `get_dmarc_record()`.
    This will fetch the DMARC record for the specified hostname (or the
    organizational domain, if not available on that hostname) and return
    a parsed version. This data is cached by default, preventing unnecessary
    subsequent requests to DNS.
    
    In order to fetch the organization domain, we have to consult the Public
    Suffix List from publicsuffix.org. This is a set of rules that match an
    arbitrary subdomain (like foo.bar.example.com) to an organization domain
    (such as example.com). Since not all organizational domains are that
    easy to figure out, the DMARC RFC states that this list must be
    consulted. We rebuild the list when packaging or installing, shipping it
    with Djblets, and we use a third-party module to parse it.
    
    Testing Done:
    Unit tests pass.
    
    Tested the functions manually with a handful of domains, each containing
    various DMARC configurations (or not), and with and without having to fall
    back on organizational domains.
    
    Bugs closed: 4458
    
    Reviewed at https://reviews.reviewboard.org/r/8396/

--- djblets/mail/tests.py
+++ djblets/mail/tests.py
@@ -1,14 +1,249 @@
 from __future__ import unicode_literals
 
 from django.core import mail
+from django.core.cache import cache
 from django.utils.datastructures import MultiValueDict
+from dns import resolver as dns_resolver
+from dns.rdtypes.ANY.TXT import TXT
 from kgb import SpyAgency
 
+from djblets.mail.dmarc import DmarcPolicy, get_dmarc_record
 from djblets.mail.message import EmailMessage
 from djblets.testing.testcases import TestCase
 
 
 _CONSOLE_EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+class DmarcTests(SpyAgency, TestCase):
+    """Tests for DMARC support."""
+
+    def setUp(self):
+        super(DmarcTests, self).setUp()
+
+        self.dmarc_txt_records = {}
+
+        self.spy_on(dns_resolver.query, call_fake=self._dns_query)
+        cache.clear()
+
+    def test_get_dmarc_record_with_no_record(self):
+        """Testing get_dmarc_record with no DMARC record in DNS"""
+        self.assertIsNone(get_dmarc_record('example.com', use_cache=False))
+
+    def test_get_dmarc_record_with_empty(self):
+        """Testing get_dmarc_record with empty DMARC record in DNS"""
+        self.dmarc_txt_records['_dmarc.example.com'] = ''
+        self.assertIsNone(get_dmarc_record('example.com', use_cache=False))
+
+    def test_get_dmarc_record_with_org_domain(self):
+        """Testing get_dmarc_record with falling back to organizational
+        domain
+        """
+        self.dmarc_txt_records['_dmarc.example.com'] = 'v=DMARC1; p=reject;'
+
+        record = get_dmarc_record('mail.corp.example.com', use_cache=False)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.hostname, 'example.com')
+        self.assertEqual(record.policy, DmarcPolicy.REJECT)
+        self.assertEqual(record.subdomain_policy, DmarcPolicy.UNSET)
+        self.assertEqual(record.pct, 100)
+        self.assertEqual(
+            record.fields,
+            {
+                'v': 'DMARC1',
+                'p': 'reject',
+            })
+
+    def test_get_dmarc_record_with_version_only(self):
+        """Testing get_dmarc_record with DMARC record containing a DMARC
+        version only
+        """
+        self.dmarc_txt_records['_dmarc.example.com'] = 'v=DMARC1'
+
+        record = get_dmarc_record('example.com', use_cache=False)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.hostname, 'example.com')
+        self.assertEqual(record.policy, DmarcPolicy.UNSET)
+        self.assertEqual(record.subdomain_policy, DmarcPolicy.UNSET)
+        self.assertEqual(record.pct, 100)
+        self.assertEqual(record.fields, {'v': 'DMARC1'})
+
+    def test_get_dmarc_record_with_p_none(self):
+        """Testing get_dmarc_record with DMARC record with p=none"""
+        self.dmarc_txt_records['_dmarc.example.com'] = 'v=DMARC1; p=none;'
+
+        record = get_dmarc_record('example.com', use_cache=False)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.hostname, 'example.com')
+        self.assertEqual(record.policy, DmarcPolicy.NONE)
+        self.assertEqual(record.subdomain_policy, DmarcPolicy.UNSET)
+        self.assertEqual(record.pct, 100)
+        self.assertEqual(
+            record.fields,
+            {
+                'v': 'DMARC1',
+                'p': 'none',
+            })
+
+    def test_get_dmarc_record_with_p_quarantine(self):
+        """Testing get_dmarc_record with DMARC record with p=quarantine"""
+        self.dmarc_txt_records['_dmarc.example.com'] = \
+            'v=DMARC1; p=quarantine; pct=20'
+
+        record = get_dmarc_record('example.com', use_cache=False)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.hostname, 'example.com')
+        self.assertEqual(record.policy, DmarcPolicy.QUARANTINE)
+        self.assertEqual(record.subdomain_policy, DmarcPolicy.UNSET)
+        self.assertEqual(record.pct, 20)
+        self.assertEqual(
+            record.fields,
+            {
+                'v': 'DMARC1',
+                'p': 'quarantine',
+                'pct': '20',
+            })
+
+    def test_get_dmarc_record_with_p_reject(self):
+        """Testing get_dmarc_record with DMARC record with p=reject"""
+        self.dmarc_txt_records['_dmarc.example.com'] = \
+            'v=DMARC1; p=reject; pct=100'
+
+        record = get_dmarc_record('example.com', use_cache=False)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.hostname, 'example.com')
+        self.assertEqual(record.policy, DmarcPolicy.REJECT)
+        self.assertEqual(record.subdomain_policy, DmarcPolicy.UNSET)
+        self.assertEqual(record.pct, 100)
+        self.assertEqual(
+            record.fields,
+            {
+                'v': 'DMARC1',
+                'p': 'reject',
+                'pct': '100',
+            })
+
+    def test_get_dmarc_record_with_sp_none(self):
+        """Testing get_dmarc_record with DMARC record with sp=none"""
+        self.dmarc_txt_records['_dmarc.example.com'] = \
+            'v=DMARC1; p=quarantine; sp=none;'
+
+        record = get_dmarc_record('example.com', use_cache=False)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.hostname, 'example.com')
+        self.assertEqual(record.policy, DmarcPolicy.QUARANTINE)
+        self.assertEqual(record.subdomain_policy, DmarcPolicy.NONE)
+        self.assertEqual(record.pct, 100)
+        self.assertEqual(
+            record.fields,
+            {
+                'v': 'DMARC1',
+                'p': 'quarantine',
+                'sp': 'none',
+            })
+
+    def test_get_dmarc_record_with_sp_quarantine(self):
+        """Testing get_dmarc_record with DMARC record with sp=quarantine"""
+        self.dmarc_txt_records['_dmarc.example.com'] = \
+            'v=DMARC1; p=quarantine; sp=quarantine;'
+
+        record = get_dmarc_record('example.com', use_cache=False)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.hostname, 'example.com')
+        self.assertEqual(record.policy, DmarcPolicy.QUARANTINE)
+        self.assertEqual(record.subdomain_policy, DmarcPolicy.QUARANTINE)
+        self.assertEqual(record.pct, 100)
+        self.assertEqual(
+            record.fields,
+            {
+                'v': 'DMARC1',
+                'p': 'quarantine',
+                'sp': 'quarantine',
+            })
+
+    def test_get_dmarc_record_with_sp_reject(self):
+        """Testing get_dmarc_record with DMARC record with p=reject"""
+        self.dmarc_txt_records['_dmarc.example.com'] = \
+            'v=DMARC1; p=quarantine; sp=reject;'
+
+        record = get_dmarc_record('example.com', use_cache=False)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.hostname, 'example.com')
+        self.assertEqual(record.policy, DmarcPolicy.QUARANTINE)
+        self.assertEqual(record.subdomain_policy, DmarcPolicy.REJECT)
+        self.assertEqual(record.pct, 100)
+        self.assertEqual(
+            record.fields,
+            {
+                'v': 'DMARC1',
+                'p': 'quarantine',
+                'sp': 'reject',
+            })
+
+    def test_get_dmarc_record_with_different_whitespace(self):
+        """Testing get_dmarc_record with DMARC record with different amounts
+        of whitespace in record
+        """
+        self.dmarc_txt_records['_dmarc.example.com'] = \
+            'v=DMARC1;p=quarantine;  sp = reject; '
+
+        record = get_dmarc_record('example.com', use_cache=False)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.hostname, 'example.com')
+        self.assertEqual(record.policy, DmarcPolicy.QUARANTINE)
+        self.assertEqual(record.subdomain_policy, DmarcPolicy.REJECT)
+        self.assertEqual(record.pct, 100)
+        self.assertEqual(
+            record.fields,
+            {
+                'v': 'DMARC1',
+                'p': 'quarantine',
+                'sp': 'reject',
+            })
+
+    def test_get_dmarc_record_with_bad_format(self):
+        """Testing get_dmarc_record with DMARC record with bad format issues"""
+        self.dmarc_txt_records['_dmarc.example.com'] = \
+            'v=DMARC1; p=quarantine; ; =value; a=b=c; sp='
+
+        record = get_dmarc_record('example.com', use_cache=False)
+        self.assertIsNotNone(record)
+        self.assertEqual(record.hostname, 'example.com')
+        self.assertEqual(record.policy, DmarcPolicy.QUARANTINE)
+        self.assertEqual(record.subdomain_policy, DmarcPolicy.UNSET)
+        self.assertEqual(record.pct, 100)
+        self.assertEqual(
+            record.fields,
+            {
+                'v': 'DMARC1',
+                'p': 'quarantine',
+                'a': 'b=c',
+            })
+
+    def test_get_dmarc_record_with_cache(self):
+        """Testing get_dmarc_record with use_cache=True"""
+        self.dmarc_txt_records['_dmarc.example.com'] = 'v=DMARC1; p=reject;'
+
+        record1 = get_dmarc_record('example.com', use_cache=True)
+        self.assertIsNotNone(record1)
+        self.assertEqual(record1.hostname, 'example.com')
+        self.assertEqual(
+            record1.fields,
+            {
+                'v': 'DMARC1',
+                'p': 'reject',
+            })
+
+        record2 = get_dmarc_record('example.com', use_cache=True)
+        self.assertEqual(record1, record2)
+
+        self.assertEqual(len(dns_resolver.query.spy.calls), 1)
+
+    def _dns_query(self, qname, rdtype, *args, **kwargs):
+        try:
+            return [TXT(1, 16, [self.dmarc_txt_records[qname]])]
+        except KeyError:
+            raise dns_resolver.NXDOMAIN
 
 
 class EmailTests(SpyAgency, TestCase):
