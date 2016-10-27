4d244df98b98e61a1a772d23ffe7511ec30c1536
Christian Hammond <christian@beanbaginc.com>
2016-10-27 16:33:10 -0700

    Clean up and document the Djblets setup.py file.
    This is a cleanup change for bringing setup.py back to our modern coding
    standards and to help with maintenance. It adds docstrings where
    missing, removes the outdated ez_setup.py support, switches strings to
    single quotes, appends "Command" to all command classes, removes the
    support for the ancient "nightlies" download page, and fixes up the
    setup() call to be better organized and use a more sane indent level.
    The Djblets package also has a better description.
    
    Testing Done:
    Tested package building and `develop` mode.
    
    Reviewed at https://reviews.reviewboard.org/r/8497/

--- setup.py
+++ setup.py
@@ -1,28 +1,4 @@
 #!/usr/bin/env python
-#
-# setup.py -- Installation for djblets
-#
-# Copyright (C) 2008 Christian Hammond
-# Copyright (C) 2007-2008 David Trowbridge
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import json
 import os
@@ -30,18 +6,12 @@ import subprocess
 import sys
 from distutils.core import Command
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 from setuptools.command.develop import develop
 from setuptools.command.egg_info import egg_info
 from setuptools.command.test import test
 
-from djblets import get_package_version, is_release, VERSION
+from djblets import get_package_version, VERSION
 from djblets.dependencies import (build_dependency_list, npm_dependencies,
                                   package_dependencies)
 
@@ -55,8 +25,16 @@ if sys.hexversion < 0x02060000:
     sys.exit(1)
 
 
-class BuildEggInfo(egg_info):
+class BuildEggInfoCommand(egg_info):
+    """Build the egg information for the package.
+
+    If this is called when building a distribution (source, egg, or wheel),
+    or when installing the package from source, this will kick off tasks for
+    building static media and string localization files.
+    """
+
     def run(self):
+        """Build the egg information."""
         if ('sdist' in sys.argv or
             'bdist_egg' in sys.argv or
             'bdist_wheel' in sys.argv or
@@ -75,13 +53,13 @@ class DevelopCommand(develop):
     Python module search path. That includes updating the versions of pip
     and setuptools on the system.
 
-    To speed up subsequent runs, callers can pass --no-npm to prevent
+    To speed up subsequent runs, callers can pass ``--no-npm`` to prevent
     installing node.js packages.
     """
 
     user_options = develop.user_options + [
         ('no-npm', None, "Don't install packages from npm"),
-        ('use-npm-cache', None, "Use npm-cache to install packages"),
+        ('use-npm-cache', None, 'Use npm-cache to install packages'),
     ]
 
     boolean_options = develop.boolean_options + ['no-npm', 'use-npm-cache']
@@ -150,16 +128,35 @@ class DevelopCommand(develop):
             raise RuntimeError('Failed to run `%s`' % cmd)
 
 
-class BuildMedia(Command):
+class BuildMediaCommand(Command):
+    """Builds static media files for the package.
+
+    This requires first having the node.js dependencies installed.
+    """
+
     user_options = []
 
     def initialize_options(self):
+        """Initialize options for the command.
+
+        This is required, but does not actually do anything.
+        """
         pass
 
     def finalize_options(self):
+        """Finalize options for the command.
+
+        This is required, but does not actually do anything.
+        """
         pass
 
     def run(self):
+        """Runs the commands to build the static media files.
+
+        Raises:
+            RuntimeError:
+                Static media failed to build.
+        """
         retcode = subprocess.call([
             sys.executable, 'contrib/internal/build-media.py'])
 
@@ -167,27 +164,44 @@ class BuildMedia(Command):
             raise RuntimeError('Failed to build media files')
 
 
-class BuildI18n(Command):
+class BuildI18nCommand(Command):
+    """Builds string localization files."""
+
     description = 'Compile message catalogs to .mo'
     user_options = []
 
     def initialize_options(self):
+        """Initialize options for the command.
+
+        This is required, but does not actually do anything.
+        """
         pass
 
     def finalize_options(self):
+        """Finalize options for the command.
+
+        This is required, but does not actually do anything.
+        """
         pass
 
     def run(self):
+        """Runs the commands to build the string localization files.
+
+        Raises:
+            RuntimeError:
+                Localization files failed to build.
+        """
         # If we are attempting to build on a system without an
         # existing copy of Djblets installed in a reachable
         # location (such as distribution packaging), we need to
         # ensure that the source directory is in the PYTHONPATH
         # or the import of djblets.util.filesystem will fail.
-        current_path = os.getenv("PYTHONPATH")
+        current_path = os.getenv('PYTHONPATH')
+
         if current_path:
-            os.putenv("PYTHONPATH", "%s:%s" % (current_path, os.getcwd()))
+            os.putenv('PYTHONPATH', '%s:%s' % (current_path, os.getcwd()))
         else:
-            os.putenv("PYTHONPATH", os.getcwd())
+            os.putenv('PYTHONPATH', os.getcwd())
 
         retcode = subprocess.call([
             sys.executable, 'contrib/internal/build-i18n.py'])
@@ -197,7 +211,7 @@ class BuildI18n(Command):
 
 
 class InstallNodeDependenciesCommand(Command):
-    """Install all node.js dependencies from npm.
+    """Installs all node.js dependencies from npm.
 
     If ``--use-npm-cache`` is passed, this will use :command:`npm-cache`
     to install the packages, which is best for Continuous Integration setups.
@@ -208,7 +222,7 @@ class InstallNodeDependenciesCommand(Command):
         'Install the node packages required for building static media.'
 
     user_options = [
-        ('use-npm-cache', None, "Use npm-cache to install packages"),
+        ('use-npm-cache', None, 'Use npm-cache to install packages'),
     ]
 
     boolean_options = ['use-npm-cache']
@@ -264,48 +278,49 @@ class InstallNodeDependenciesCommand(Command):
                 'One or more node.js modules could not be installed.')
 
 
+# Tell `setup.py tests` how to invoke our test suite.
 test.run_tests = lambda *args, **kwargs: os.system('tests/runtests.py')
-
-cmdclasses = {
-    'develop': DevelopCommand,
-    'egg_info': BuildEggInfo,
-    'build_media': BuildMedia,
-    'build_i18n': BuildI18n,
-    'install_node_deps': InstallNodeDependenciesCommand,
-}
 
 
 PACKAGE_NAME = 'Djblets'
 
-if is_release():
-    download_url = 'http://downloads.reviewboard.org/releases/%s/%s.%s/' % \
-                   (PACKAGE_NAME, VERSION[0], VERSION[1])
-else:
-    download_url = 'http://downloads.reviewboard.org/nightlies/'
-
-
-setup(name=PACKAGE_NAME,
-      version=get_package_version(),
-      test_suite="dummy",
-      license="MIT",
-      description="A collection of useful classes and functions for Django",
-      packages=find_packages(exclude=["tests"]),
-      cmdclass=cmdclasses,
-      install_requires=build_dependency_list(package_dependencies),
-      include_package_data=True,
-      zip_safe=False,
-      maintainer="Christian Hammond",
-      maintainer_email="christian@beanbaginc.com",
-      url="https://www.reviewboard.org/downloads/djblets/",
-      download_url=download_url,
-      classifiers=[
-          "Development Status :: 4 - Beta",
-          "Environment :: Web Environment",
-          "Framework :: Django",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Topic :: Software Development",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ])
+setup(
+    name=PACKAGE_NAME,
+    version=get_package_version(),
+    license='MIT',
+    description=(
+        'A collection of useful classes and functions for developing '
+        'large-scale Django-based web applications.'
+    ),
+    author='Beanbag, Inc.',
+    author_email='reviewboard@googlegroups.com',
+    url='https://www.reviewboard.org/downloads/djblets/',
+    download_url=('https://downloads.reviewboard.org/releases/%s/%s.%s/'
+                  % (PACKAGE_NAME, VERSION[0], VERSION[1])),
+    packages=find_packages(exclude=['tests']),
+    install_requires=build_dependency_list(package_dependencies),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='dummy',
+    cmdclass={
+        'develop': DevelopCommand,
+        'egg_info': BuildEggInfoCommand,
+        'build_media': BuildMediaCommand,
+        'build_i18n': BuildI18nCommand,
+        'install_node_deps': InstallNodeDependenciesCommand,
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
