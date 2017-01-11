a8ac028185105ba180eb96ad486d737da8740247
Christian Hammond <christian@beanbaginc.com>
2017-01-11 04:22:45 -0800

    Backport the extension templatetags unit tests to 0.8.
    The master branch introduced unit tests for the extension templatetags,
    along with a series of files and settings needed to run them. These are
    useful on older branches. This change backports those features to
    release-0.8.x and newer branches.

--- tests/settings.py
+++ tests/settings.py
@@ -68,10 +68,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'djblets.testing.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or
-    # "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates')),
 )
 
 INSTALLED_APPS = [
