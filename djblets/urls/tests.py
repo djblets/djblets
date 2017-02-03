b22fecbb8e1d56990ae99eb9c3f891b0ef895e6e
Christian Hammond <christian@beanbaginc.com>
2017-02-03 14:07:11 -0800

    Fix bad state assumptions in the DynamicURLResolver tests.
    The DynamicURLResolver tests had an order dependency issue. They assumed
    that no prior test would populate the URL resolution caches. This was a
    terrible assumption, and the recent extension work broke this.
    
    We now make sure to clear the URL caches before and after the test,
    fixing this particular problem. Some of the inner workings of the test
    have also been cleaned up, making the test more self-contained.
    
    Testing Done:
    All unit tests pass on Django 1.6 and 1.8 through 1.10.
    
    Reviewed at https://reviews.reviewboard.org/r/8708/

--- djblets/urls/tests.py
+++ djblets/urls/tests.py
@@ -1,53 +1,53 @@
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.conf.urls import include, url
-from django.core.urlresolvers import NoReverseMatch, reverse
+from django.core.urlresolvers import (NoReverseMatch, clear_url_caches,
+                                      reverse)
 
 from djblets.testing.testcases import TestCase
 from djblets.urls.resolvers import DynamicURLResolver
 
 
 class URLResolverTests(TestCase):
-    def setUp(self):
-        self._old_root_urlconf = settings.ROOT_URLCONF
-
     def tearDown(self):
-        settings.ROOT_URLCONF = self._old_root_urlconf
+        super(URLResolverTests, self).tearDown()
+        clear_url_caches()
 
     def test_dynamic_url_resolver(self):
         """Testing DynamicURLResolver"""
-        self.dynamic_urls = DynamicURLResolver()
+        def dummy_view(self):
+            pass
 
-        settings.ROOT_URLCONF = [
-            url(r'^root/', include([self.dynamic_urls])),
-            url(r'^foo/', self._dummy_view, name='foo'),
+        dynamic_urls = DynamicURLResolver()
+        root_urlconf = [
+            url(r'^root/', include([dynamic_urls])),
+            url(r'^foo/', dummy_view, name='foo'),
         ]
 
-        new_patterns = [
-            url(r'^bar/$', self._dummy_view, name='bar'),
-            url(r'^baz/$', self._dummy_view, name='baz'),
-        ]
+        with self.settings(ROOT_URLCONF=root_urlconf):
+            clear_url_caches()
 
-        # The new patterns shouldn't reverse, just the original "foo".
-        reverse('foo')
-        self.assertRaises(NoReverseMatch, reverse, 'bar')
-        self.assertRaises(NoReverseMatch, reverse, 'baz')
+            new_patterns = [
+                url(r'^bar/$', dummy_view, name='bar'),
+                url(r'^baz/$', dummy_view, name='baz'),
+            ]
 
-        # Add the new patterns. Now reversing should work.
-        self.dynamic_urls.add_patterns(new_patterns)
+            # The new patterns shouldn't reverse, just the original "foo".
+            reverse('foo')
+            self.assertRaises(NoReverseMatch, reverse, 'bar')
+            self.assertRaises(NoReverseMatch, reverse, 'baz')
 
-        reverse('foo')
-        reverse('bar')
-        reverse('baz')
+            # Add the new patterns. Now reversing should work.
+            dynamic_urls.add_patterns(new_patterns)
 
-        # Get rid of the patterns again. We should be back in the original
-        # state.
-        self.dynamic_urls.remove_patterns(new_patterns)
+            reverse('foo')
+            reverse('bar')
+            reverse('baz')
 
-        reverse('foo')
-        self.assertRaises(NoReverseMatch, reverse, 'bar')
-        self.assertRaises(NoReverseMatch, reverse, 'baz')
+            # Get rid of the patterns again. We should be back in the original
+            # state.
+            dynamic_urls.remove_patterns(new_patterns)
 
-    def _dummy_view(self):
-        pass
+            reverse('foo')
+            self.assertRaises(NoReverseMatch, reverse, 'bar')
+            self.assertRaises(NoReverseMatch, reverse, 'baz')
