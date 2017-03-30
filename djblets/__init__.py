de9cdb418b1ee448a62dbd9cb612b4cc7a58668b
Christian Hammond <christian@beanbaginc.com>
2017-03-29 22:31:43 -0700

    Release Djblets 0.10 beta 1.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 10, 0, 'alpha', 0, False)
+VERSION = (0, 10, 0, 'beta', 1, True)
 
 
 def get_version_string():
