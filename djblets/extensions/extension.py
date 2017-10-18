bfad90560096b7ab1b144fde8e79fd12ea636374
Christian Hammond <christian@beanbaginc.com>
2017-10-18 13:00:18 -0700

    Document and clean up the ExtensionManager code.
    This change is primarily concerned with documenting `ExtensionManager`,
    fleshing out some details of some of the methods, in preparation for
    some new work.
    
    Some of the code has been cleaned up a bit just to better conform with
    coding standards. Double quotes have been turned into single quotes,
    line lengths are more consistent, and some dictionary key checks have
    been turned into `try/except` blocks. The logic changes are minimal.
    
    Testing Done:
    Unit tests pass.
    
    Checked to make sure all common extension operations (loading, enabling,
    disabling, configuring) worked.
    
    Checked that docs built without errors.
    
    Reviewed at https://reviews.reviewboard.org/r/9272/

--- djblets/extensions/extension.py
+++ djblets/extensions/extension.py
@@ -1,27 +1,4 @@
-#
-# extension.py -- Base classes for extensions
-#
-# Copyright (c) 2010-2013  Beanbag, Inc.
-# Copyright (c) 2008-2010  Christian Hammond
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""Base classes for implementing extensions."""
 
 from __future__ import unicode_literals
 
@@ -362,7 +339,6 @@ class Extension(object):
         This ID should be used when manually referencing the bundle in a
         template. The ID will be unique across all extensions.
 
-
         Args:
             name (unicode):
                 The name of the bundle to reference.
@@ -568,4 +544,4 @@ class ExtensionInfo(object):
         self.author_url = metadata.get('Author-home-page', self.url)
 
     def __str__(self):
-        return "%s %s (enabled = %s)" % (self.name, self.version, self.enabled)
+        return '%s %s (enabled = %s)' % (self.name, self.version, self.enabled)
