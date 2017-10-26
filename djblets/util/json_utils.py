9ef1fbe2b9d230beda3ea68465a5c5b6f414ce6e
Christian Hammond <christian@beanbaginc.com>
2017-10-26 02:10:58 -0700

    Add utility functions for working with JSON Pointer paths.
    This introduces two new JSON utility functions, `json_get_pointer_info`
    and `json_resolve_pointer`, which are used to return information from a
    JSON-compatible object based on a JSON Pointer path. These paths are a
    standard way of specifying a location within an object, supporting
    dictionary keys, array indices, and even an "end of array" marker (for
    insertion purposes in other functions).
    
    `json_get_pointer_info` attempts to resolve as much of the specified
    path as possible, returning information on what was able to be resolved,
    what remains, and what error should be shown if expecting a
    fully-resolvable path. It's useful for cases where the path being used
    may not necessarily have to represent an existing location in an object.
    
    `json_resolve_pointer` takes a fully-resolvable Pointer path and returns
    the value from the object that it matches. If the path cannot be
    resolved, an exception is thrown. This is intended for use in the more
    common cases of simply needing to access data from an object.
    
    Testing Done:
    Unit tests passed.
    
    Reviewed at https://reviews.reviewboard.org/r/9263/

--- djblets/util/json_utils.py
+++ djblets/util/json_utils.py
@@ -1,10 +1,93 @@
 from __future__ import unicode_literals
 
 from django.utils import six
+from django.utils.translation import ugettext as _
 
 
 class JSONPatchError(Exception):
     """An error occurred while patching an object."""
+
+
+class JSONPointerError(Exception):
+    """Base class for JSON Pointer errors."""
+
+
+class JSONPointerSyntaxError(JSONPointerError):
+    """Syntax error in a JSON Pointer path."""
+
+
+class JSONPointerLookupError(JSONPointerError):
+    """Error looking up data from a JSON Pointer path."""
+
+    def __init__(self, msg, parent, token, token_index, tokens):
+        """Initialize the error.
+
+        Args:
+            msg (unicode):
+                The error message.
+
+            parent (object):
+                The parent object. This may be a dictionary or a list.
+
+            token (unicode):
+                The last resolvable token in the path. This will be ``None``
+                if the first token failed.
+
+            token_index (int):
+                The 0-based index of the last resolvable token in the path.
+                This will be ``None`` if the first token failed.
+
+            tokens (list of unicode):
+                The list of tokens comprising the full path.
+        """
+        super(JSONPointerLookupError, self).__init__(msg)
+
+        self.parent = parent
+        self.token = token
+        self.token_index = token_index
+        self.tokens = tokens
+
+
+class JSONPointerEndOfList(object):
+    """A representation of the end of a list.
+
+    This is used by the JSON Pointer functions to represent the very end of
+    a list (not the last item). This is used primarily when specifying an
+    insertion point, with this value repersenting appending to a list.
+    """
+
+    def __init__(self, json_list):
+        """Initialize the object.
+
+        Args:
+            json_list (list):
+                The list this object represents.
+        """
+        self.json_list = json_list
+
+    def __eq__(self, other_list):
+        """Return whether two instances are equal.
+
+        Instances are equal if both of their lists are equal.
+
+        Args:
+            other_list (JSONPointerEndOfList):
+                The other instance to compare to for equality.
+
+        Returns:
+            bool:
+            ``True`` if the two lists are equal.
+        """
+        return self.json_list == other_list.json_list
+
+    def __repr__(self):
+        """Return a string representation of the instance.
+
+        Returns:
+            unicode:
+            A string representation of the instance.
+        """
+        return 'JSONPointerEndOfList<%r>' % self.json_list
 
 
 def json_merge_patch(doc, patch, can_write_key_func=None):
@@ -106,3 +189,209 @@ def json_merge_patch(doc, patch, can_write_key_func=None):
         raise ValueError('can_write_key_func must be callable')
 
     return _merge_patch(doc, patch)
+
+
+def json_get_pointer_info(obj, path):
+    """Return information from a JSON object based on a JSON Pointer path.
+
+    JSON Pointers are a standard way of specifying a path to data within a
+    JSON object. This method takes a JSON Pointer path and returns information
+    from the given object based on that path.
+
+    Pointer paths consist of dictionary keys, array indexes, or a special ``-``
+    token (representing the end of a list, after the last item) separated by
+    ``/`` tokens. There are also two special escaped values: ``~0``,
+    representing a ``~`` character, and ``~1``, representing a ``/`` character.
+
+    Paths must start with ``/``, or must be an empty string (which will match
+    the provided object). If the path has a trailing ``/``, then the final
+    token is actually an empty string (matching an empty key in a dictionary).
+
+    If the Pointer does not resolve to a complete path (for instance, a key
+    specified in the path is missing), the resulting information will return
+    the keys that could be resolved, keys that could not be resolved, the
+    object where it left off, and an error message. This allows implementations
+    to determine which part of a path does not yet exist, potentially for the
+    purpose of inserting data at that key in the path.
+
+    Args:
+        obj (object or list):
+            The object or list representing the starting object for the path.
+
+        path (unicode):
+            The Pointer path for the lookup.
+
+    Returns:
+        dict:
+        Information about the object and what the path was able to match. This
+        has the following keys:
+
+        ``value`` (:py:class:`object`):
+            The resulting value from the path, if the path was fully resolved.
+
+            This will be a :py:class:`JSONPointerEndOfList` if the last part of
+            the path was a ``-`` token (representing the very end of
+            a list).
+
+        ``tokens`` (:py:class:`list`):
+            The normalized (unescaped) list of path tokens that comprise the
+            full path.
+
+        ``parent`` (:py:class:`object`):
+            The parent object for either the most-recently resolved value.
+
+        ``resolved`` (:py:class:`list`):
+            The list of resolved objects from the original JSON object. This
+            will contain each key, array item, or other value that was found,
+            in path order.
+
+        ``lookup_error`` (:py:class:`unicode`):
+            The error message, if any, if failing to resolve the full path.
+
+    Raises:
+        JSONPointerSyntaxError:
+            There was a syntax error with a token in the path.
+    """
+    if path != '' and not path.startswith('/'):
+        raise JSONPointerSyntaxError(
+            _('Paths must either be empty or start with a "/"'))
+
+    # Split the path into segments, trimming off the first entry (the root
+    # object). If path is empty, the split will be a no-op, and we'll end up
+    # operating on the root object itself.
+    tokens = path.split('/')[1:]
+
+    # Decode the special values for "/" and "~". The decode order here is
+    # important.
+    norm_tokens = [
+        token.replace('~1', '/').replace('~0', '~')
+        for token in tokens
+    ]
+
+    resolved = [obj]
+    lookup_error = None
+    parent = None
+
+    for i, token in enumerate(norm_tokens):
+        parent = obj
+
+        if isinstance(obj, dict):
+            try:
+                obj = obj[token]
+            except KeyError:
+                lookup_error = (
+                    _('Dictionary key "%(key)s" not found in "%(path)s"')
+                    % {
+                        'key': token,
+                        'path': '/%s' % '/'.join(tokens[:i]),
+                    }
+                )
+        elif isinstance(obj, list):
+            if token == '-':
+                obj = JSONPointerEndOfList(obj)
+            else:
+                if token != '0' and token.startswith('0'):
+                    raise JSONPointerSyntaxError(
+                        _('List index "%s" must not begin with "0"') % token)
+
+                try:
+                    token = int(token)
+                except ValueError:
+                    raise JSONPointerSyntaxError(
+                        _('%(index)r is not a valid list index in "%(path)s"')
+                        % {
+                            'index': token,
+                            'path': '/%s' % '/'.join(tokens[:i]),
+                        })
+
+                if token < 0:
+                    raise JSONPointerSyntaxError(
+                        _('Negative indexes into lists are not allowed'))
+
+                try:
+                    obj = obj[token]
+                except IndexError:
+                    lookup_error = (
+                        _('%(index)d is outside the list in "%(path)s"')
+                        % {
+                            'index': token,
+                            'path': '/%s' % '/'.join(tokens[:i]),
+                        }
+                    )
+        else:
+            lookup_error = (
+                _('Cannot resolve path within unsupported type "%(type)s" at '
+                  '"%(path)s"')
+                % {
+                    'type': type(obj).__name__,
+                    'path': '/%s' % '/'.join(tokens[:i]),
+                }
+            )
+
+        if lookup_error:
+            obj = None
+            break
+
+        resolved.append(obj)
+
+    return {
+        'value': obj,
+        'parent': parent,
+        'all_tokens': norm_tokens,
+        'resolved_values': resolved,
+        'resolved_tokens': norm_tokens[:len(resolved) - 1],
+        'unresolved_tokens': norm_tokens[len(resolved) - 1:],
+        'lookup_error': lookup_error,
+    }
+
+
+def json_resolve_pointer(obj, path):
+    """Return the value from a JSON object based on a JSON Pointer path.
+
+    See :py:func:`json_get_pointer_info` for information on how a Pointer
+    path is constructed. Unlike that function, this requires a fully-resolved
+    path.
+
+    Args:
+        obj (object or list):
+            The object or list representing the starting object for the path.
+
+        path (unicode):
+            The Pointer path for the lookup.
+
+    Returns:
+        object:
+        The resulting value from the object, based on the path.
+
+        This will be a :py:class:`JSONPointerEndOfList` if the last part of
+        the path was a ``-`` token (representing the very end of
+        a list).
+
+    Raises:
+        JSONPointerLookupError:
+            The path could not be fully resolved.
+
+        JSONPointerSyntaxError:
+            There was a syntax error with a token in the path.
+    """
+    info = json_get_pointer_info(obj, path)
+    lookup_error = info['lookup_error']
+
+    if lookup_error:
+        tokens = info['all_tokens']
+        parent = info['parent']
+
+        if parent is None:
+            token_index = None
+            token = None
+        else:
+            token_index = len(info['resolved_tokens']) - 1
+            token = tokens[token_index]
+
+        raise JSONPointerLookupError(lookup_error,
+                                     parent=parent,
+                                     token=token,
+                                     token_index=token_index,
+                                     tokens=tokens)
+
+    return info['value']
--- djblets/util/tests/test_json_utils.py
+++ djblets/util/tests/test_json_utils.py
@@ -3,7 +3,13 @@
 from __future__ import unicode_literals
 
 from djblets.testing.testcases import TestCase
-from djblets.util.json_utils import JSONPatchError, json_merge_patch
+from djblets.util.json_utils import (JSONPatchError,
+                                     JSONPointerEndOfList,
+                                     JSONPointerLookupError,
+                                     JSONPointerSyntaxError,
+                                     json_get_pointer_info,
+                                     json_merge_patch,
+                                     json_resolve_pointer)
 
 
 class JSONMergePathTests(TestCase):
@@ -255,3 +261,517 @@ class JSONMergePathTests(TestCase):
                 'd': True,
             },
         })
+
+
+class JSONGetPointerInfoTests(TestCase):
+    """Unit tests for djblets.util.json_utils.json_get_pointer_info."""
+
+    def test_with_relative_path(self):
+        """Testing json_get_pointer_info with relative path"""
+        message = 'Paths must either be empty or start with a "/"'
+
+        with self.assertRaisesMessage(JSONPointerSyntaxError, message):
+            json_get_pointer_info({}, 'a/')
+
+    def test_with_dict(self):
+        """Testing json_get_pointer_info with valid dictionary path"""
+        obj = {
+            '': 'hi',
+            'a': {
+                'b': {
+                    'c': 123,
+                },
+            },
+        }
+
+        self.assertEqual(
+            json_get_pointer_info(obj, ''),
+            {
+                'value': obj,
+                'parent': None,
+                'resolved_values': [obj],
+                'all_tokens': [],
+                'resolved_tokens': [],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+        self.assertEqual(
+            json_get_pointer_info(obj, '/'),
+            {
+                'value': 'hi',
+                'parent': obj,
+                'resolved_values': [obj, 'hi'],
+                'all_tokens': [''],
+                'resolved_tokens': [''],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a'),
+            {
+                'value': obj['a'],
+                'parent': obj,
+                'resolved_values': [obj, obj['a']],
+                'all_tokens': ['a'],
+                'resolved_tokens': ['a'],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a/b'),
+            {
+                'value': obj['a']['b'],
+                'parent': obj['a'],
+                'resolved_values': [obj,
+                                    obj['a'],
+                                    obj['a']['b']],
+                'all_tokens': ['a', 'b'],
+                'resolved_tokens': ['a', 'b'],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a/b/c'),
+            {
+                'value': obj['a']['b']['c'],
+                'parent': obj['a']['b'],
+                'resolved_values': [obj,
+                                    obj['a'],
+                                    obj['a']['b'],
+                                    obj['a']['b']['c']],
+                'all_tokens': ['a', 'b', 'c'],
+                'resolved_tokens': ['a', 'b', 'c'],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+
+    def test_with_list_item(self):
+        """Testing json_get_pointer_info with valid resulting list item"""
+        obj = {
+            'a': {
+                'b': {
+                    'c': [1, 2, 3],
+                },
+            },
+        }
+
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a/b/c/0'),
+            {
+                'value': 1,
+                'parent': obj['a']['b']['c'],
+                'resolved_values': [obj,
+                                    obj['a'],
+                                    obj['a']['b'],
+                                    obj['a']['b']['c'],
+                                    obj['a']['b']['c'][0]],
+                'all_tokens': ['a', 'b', 'c', '0'],
+                'resolved_tokens': ['a', 'b', 'c', '0'],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a/b/c/1'),
+            {
+                'value': 2,
+                'parent': obj['a']['b']['c'],
+                'resolved_values': [obj,
+                                    obj['a'],
+                                    obj['a']['b'],
+                                    obj['a']['b']['c'],
+                                    obj['a']['b']['c'][1]],
+                'all_tokens': ['a', 'b', 'c', '1'],
+                'resolved_tokens': ['a', 'b', 'c', '1'],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a/b/c/2'),
+            {
+                'value': 3,
+                'parent': obj['a']['b']['c'],
+                'resolved_values': [obj,
+                                    obj['a'],
+                                    obj['a']['b'],
+                                    obj['a']['b']['c'],
+                                    obj['a']['b']['c'][2]],
+                'all_tokens': ['a', 'b', 'c', '2'],
+                'resolved_tokens': ['a', 'b', 'c', '2'],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+
+    def test_with_list_item_end_of_list(self):
+        """Testing json_get_pointer_info with valid resulting list item using
+        "-" for end of list
+        """
+        obj = {
+            'a': {
+                'b': {
+                    'c': [1, 2, 3],
+                },
+            },
+        }
+
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a/b/c/-'),
+            {
+                'value': JSONPointerEndOfList([1, 2, 3]),
+                'parent': obj['a']['b']['c'],
+                'resolved_values': [obj,
+                                    obj['a'],
+                                    obj['a']['b'],
+                                    obj['a']['b']['c'],
+                                    JSONPointerEndOfList([1, 2, 3])],
+                'all_tokens': ['a', 'b', 'c', '-'],
+                'resolved_tokens': ['a', 'b', 'c', '-'],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+
+    def test_with_nested_lists_dicts(self):
+        """Testing json_get_pointer_info with valid path containing nested
+        lists and dictionaries
+        """
+        obj = {
+            'a': [
+                1,
+                {
+                    'b': [2, 3, 'foo'],
+                },
+            ],
+        }
+
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a/1/b/2'),
+            {
+                'value': 'foo',
+                'parent': obj['a'][1]['b'],
+                'resolved_values': [obj,
+                                    obj['a'],
+                                    obj['a'][1],
+                                    obj['a'][1]['b'],
+                                    obj['a'][1]['b'][2]],
+                'all_tokens': ['a', '1', 'b', '2'],
+                'resolved_tokens': ['a', '1', 'b', '2'],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+
+    def test_with_escaped(self):
+        """Testing json_get_pointer_info with valid escaped paths"""
+        obj = {
+            '/': {
+                '~': {
+                    '~/~': 123,
+                },
+            },
+        }
+
+        self.assertEqual(
+            json_get_pointer_info(obj, '/~1/~0/~0~1~0'),
+            {
+                'value': 123,
+                'parent': obj['/']['~'],
+                'resolved_values': [obj,
+                                    obj['/'],
+                                    obj['/']['~'],
+                                    obj['/']['~']['~/~']],
+                'all_tokens': ['/', '~', '~/~'],
+                'resolved_tokens': ['/', '~', '~/~'],
+                'unresolved_tokens': [],
+                'lookup_error': None,
+            })
+
+    def test_with_bad_key(self):
+        """Testing json_get_pointer_info with invalid path containing a bad
+        key
+        """
+        obj = {
+            'a': {
+                'b': 123,
+            },
+        }
+
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a/c'),
+            {
+                'value': None,
+                'parent': obj['a'],
+                'resolved_values': [obj,
+                                    obj['a']],
+                'all_tokens': ['a', 'c'],
+                'resolved_tokens': ['a'],
+                'unresolved_tokens': ['c'],
+                'lookup_error': 'Dictionary key "c" not found in "/a"',
+            })
+
+    def test_with_bad_list_index(self):
+        """Testing json_get_pointer_info with invalid path containing a bad
+        list index
+        """
+        obj = {
+            'a': {
+                'b': [1, 2],
+            },
+        }
+
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a/b/3'),
+            {
+                'value': None,
+                'parent': obj['a']['b'],
+                'resolved_values': [obj,
+                                    obj['a'],
+                                    obj['a']['b']],
+                'all_tokens': ['a', 'b', '3'],
+                'resolved_tokens': ['a', 'b'],
+                'unresolved_tokens': ['3'],
+                'lookup_error': '3 is outside the list in "/a/b"',
+            })
+
+    def test_with_non_traversable_path(self):
+        """Testing json_get_pointer_info with invalid path containing a
+        non-traversable item
+        """
+        obj = {
+            'a': {
+                'b': 'test',
+            },
+        }
+
+        self.assertEqual(
+            json_get_pointer_info(obj, '/a/b/c'),
+            {
+                'value': None,
+                'parent': obj['a']['b'],
+                'resolved_values': [obj,
+                                    obj['a'],
+                                    obj['a']['b']],
+                'all_tokens': ['a', 'b', 'c'],
+                'resolved_tokens': ['a', 'b'],
+                'unresolved_tokens': ['c'],
+                'lookup_error': (
+                    'Cannot resolve path within unsupported type '
+                    '"unicode" at "/a/b"'
+                ),
+            })
+
+    def test_with_negative_list_index(self):
+        """Testing json_get_pointer_info with invalid path containing a negative
+        list index
+        """
+        obj = {
+            'a': {
+                'b': [1, 2],
+            },
+        }
+
+        message = 'Negative indexes into lists are not allowed'
+
+        with self.assertRaisesMessage(JSONPointerSyntaxError, message):
+            json_get_pointer_info(obj, '/a/b/-1')
+
+    def test_with_index_with_leading_zero(self):
+        """Testing json_get_pointer_info with invalid path containing a list
+        index with a leading zero
+        """
+        obj = {
+            'a': {
+                'b': [1, 2],
+            },
+        }
+
+        message = 'List index "01" must not begin with "0"'
+
+        with self.assertRaisesMessage(JSONPointerSyntaxError, message):
+            json_get_pointer_info(obj, '/a/b/01')
+
+    def test_with_non_int_list_index(self):
+        """Testing json_get_pointer_info with invalid path containing a
+        non-integer list index
+        """
+        obj = {
+            'a': {
+                'b': [1, 2],
+            },
+        }
+
+        message = 'u\'c\' is not a valid list index in "/a/b"'
+
+        with self.assertRaisesMessage(JSONPointerSyntaxError, message):
+            json_get_pointer_info(obj, '/a/b/c')
+
+
+class JSONResolvePointerTests(TestCase):
+    """Unit tests for djblets.util.json_utils.json_resolve_pointer."""
+
+    def test_with_dict(self):
+        """Testing json_resolve_pointer with valid dictionary path"""
+        obj = {
+            '': 'hi',
+            'a': {
+                'b': {
+                    'c': 123,
+                },
+            },
+        }
+
+        self.assertEqual(json_resolve_pointer(obj, ''), obj)
+        self.assertEqual(json_resolve_pointer(obj, '/'), 'hi')
+        self.assertEqual(json_resolve_pointer(obj, '/a'), {
+            'b': {
+                'c': 123,
+            },
+        })
+        self.assertEqual(json_resolve_pointer(obj, '/a/b'), {
+            'c': 123,
+        })
+        self.assertEqual(json_resolve_pointer(obj, '/a/b/c'), 123)
+
+    def test_with_list_item(self):
+        """Testing json_resolve_pointer with valid resulting list item"""
+        obj = {
+            'a': {
+                'b': {
+                    'c': [1, 2, 3],
+                },
+            },
+        }
+
+        self.assertEqual(json_resolve_pointer(obj, '/a/b/c/0'), 1)
+        self.assertEqual(json_resolve_pointer(obj, '/a/b/c/1'), 2)
+        self.assertEqual(json_resolve_pointer(obj, '/a/b/c/2'), 3)
+
+    def test_with_list_item_end_of_list(self):
+        """Testing json_resolve_pointer with valid resulting list item using
+        "-" for end of list
+        """
+        obj = {
+            'a': {
+                'b': {
+                    'c': [1, 2, 3],
+                },
+            },
+        }
+
+        self.assertIsInstance(json_resolve_pointer(obj, '/a/b/c/-'),
+                              JSONPointerEndOfList)
+
+    def test_with_nested_lists_dicts(self):
+        """Testing json_resolve_pointer with valid path containing nested
+        lists and dictionaries
+        """
+        obj = {
+            'a': [
+                1,
+                {
+                    'b': [2, 3, 'foo'],
+                },
+            ],
+        }
+
+        self.assertEqual(json_resolve_pointer(obj, '/a/1/b/2'), 'foo')
+
+    def test_with_escaped(self):
+        """Testing json_resolve_pointer with valid escaped paths"""
+        obj = {
+            '/': {
+                '~': {
+                    '~/~': 123,
+                },
+            },
+        }
+
+        self.assertEqual(json_resolve_pointer(obj, '/~1/~0/~0~1~0'), 123)
+
+    def test_with_bad_key(self):
+        """Testing json_resolve_pointer with invalid path containing a bad
+        key
+        """
+        obj = {
+            'a': {
+                'b': 123,
+            },
+        }
+
+        message = 'Dictionary key "c" not found in "/a"'
+
+        with self.assertRaisesMessage(JSONPointerLookupError, message):
+            json_resolve_pointer(obj, '/a/c')
+
+    def test_with_bad_list_index(self):
+        """Testing json_resolve_pointer with invalid path containing a bad
+        list index
+        """
+        obj = {
+            'a': {
+                'b': [1, 2],
+            },
+        }
+
+        message = '3 is outside the list in "/a/b"'
+
+        with self.assertRaisesMessage(JSONPointerLookupError, message):
+            json_resolve_pointer(obj, '/a/b/3')
+
+    def test_with_non_traversable_path(self):
+        """Testing json_resolve_pointer with invalid path containing a
+        non-traversable item
+        """
+        obj = {
+            'a': {
+                'b': 'test',
+            },
+        }
+
+        message = \
+            'Cannot resolve path within unsupported type "unicode" at "/a/b"'
+
+        with self.assertRaisesMessage(JSONPointerLookupError, message):
+            json_resolve_pointer(obj, '/a/b/c')
+
+    def test_with_index_with_leading_zero(self):
+        """Testing json_resolve_pointer with invalid path containing a negative
+        list index
+        """
+        obj = {
+            'a': {
+                'b': [1, 2],
+            },
+        }
+
+        message = 'Negative indexes into lists are not allowed'
+
+        with self.assertRaisesMessage(JSONPointerSyntaxError, message):
+            json_resolve_pointer(obj, '/a/b/-1')
+
+    def test_with_negative_list_index(self):
+        """Testing json_resolve_pointer with invalid path containing a list
+        index with a leading zero
+        """
+        obj = {
+            'a': {
+                'b': [1, 2],
+            },
+        }
+
+        message = 'List index "01" must not begin with "0"'
+
+        with self.assertRaisesMessage(JSONPointerSyntaxError, message):
+            json_resolve_pointer(obj, '/a/b/01')
+
+    def test_with_non_int_list_index(self):
+        """Testing json_resolve_pointer with invalid path containing a
+        non-integer list index
+        """
+        obj = {
+            'a': {
+                'b': [1, 2],
+            },
+        }
+
+        message = 'u\'c\' is not a valid list index in "/a/b"'
+
+        with self.assertRaisesMessage(JSONPointerSyntaxError, message):
+            json_resolve_pointer(obj, '/a/b/c')
