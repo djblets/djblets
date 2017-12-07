95f9cfc0d6cdd63f1f4580063b08af64d46e5f3a
Christian Hammond <christian@beanbaginc.com>
2017-12-07 14:24:58 -0800

    Make never_cache_patterns() compatible with newer versions of Django.
    `never_cache_patterns()` is an old function of ours that applies
    Django's `@never_cache` decorator to a list of URLs. It was built to
    work like `patterns()`, which doesn't exist in modern versions of
    Django, and assumed older URL semantics like the ability to associate
    URLs with view lookup strings, which also doesn't exist anymore.
    
    This updates the function to be more future-proof. It no longer requires
    a prefix string as the first parameter, and in fact warns if a string
    that is non-empty is provided, outright failing if provided on Django
    1.10 or higher (which doesn't include prefix support).
    
    Unit tests were added to test the function with and without prefix
    strings, in a way that ensures things work properly on whatever version
    of Django is being tested against.
    
    Testing Done:
    Unit tests pass on Django 1.6, 1.9, 1.10, 1.11. Opting not to bother
    with older versions at this stage.
    
    Reviewed at https://reviews.reviewboard.org/r/9416/

--- djblets/urls/patterns.py
+++ djblets/urls/patterns.py
@@ -1,25 +1,66 @@
 from __future__ import unicode_literals
 
+import warnings
+
 from django.conf.urls import url
 from django.core.urlresolvers import RegexURLPattern
 from django.views.decorators.cache import never_cache
 
 
-def never_cache_patterns(prefix, *args):
-    """
-    Prevents any included URLs from being cached by the browser.
+def never_cache_patterns(*args):
+    """Prevent any included URLs from being cached by the browser.
 
     It's sometimes desirable not to allow browser caching for a set of URLs.
-    This can be used just like patterns().
-    """
-    pattern_list = []
-    for t in args:
-        if isinstance(t, (list, tuple)):
-            t = url(prefix=prefix, *t)
-        elif isinstance(t, RegexURLPattern):
-            t.add_prefix(prefix)
+    Any URLs passed in will have the
+    :py:func:`~django.views.decorators.cache.never_cache` decorator applied.
 
-        t._callback = never_cache(t.callback)
+    Args:
+        *args (tuple):
+            The URL arguments to pass to the function.
+
+            If the first parameter is a prefix string for view lookup strings,
+            then this will emit a deprecation warning, as these are no longer
+            supported in Django 1.10 or higher.
+
+    Returns:
+        list:
+        A list of URL patterns.
+    """
+    if isinstance(args[0], basestring):
+        prefix = args[0]
+        args = args[1:]
+    else:
+        prefix = None
+
+    if prefix:
+        msg = ('String prefixes for URLs in never_cache_patterns() is '
+               'deprecated, and will not work on Django 1.10 or higher.')
+
+        if hasattr(RegexURLPattern, 'add_prefix'):
+            warnings.warn(msg, DeprecationWarning)
+        else:
+            raise ValueError(msg)
+
+    pattern_list = []
+
+    for t in args:
+        if prefix:
+            if isinstance(t, (list, tuple)):
+                # This will crash on Django 1.10. The user has been warned.
+                t = url(prefix=prefix, *t)
+            elif isinstance(t, RegexURLPattern):
+                # This will crash on Django 1.10. The user has been warned.
+                t.add_prefix(prefix)
+
+        cb = never_cache(t.callback)
+
+        if hasattr(t, '_callback'):
+            # Django <= 1.9
+            t._callback = cb
+        else:
+            # Django >= 1.10
+            t.callback = cb
+
         pattern_list.append(t)
 
     return pattern_list
