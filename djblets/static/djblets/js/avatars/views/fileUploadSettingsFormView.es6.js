c3c3b34bea6b813a4aea6cad48fbac66e0cb19bf
Christian Hammond <christian@beanbaginc.com>
2018-02-05 12:29:43 -0800

    Redesign the File Upload avatar configuration.
    The File Upload avatar configuration didn't really fit in with the rest
    of the UI. The avatar area was pretty large, and the file input bumped
    up against the Save button. It also lacked some niceties that we offer
    elsewhere for file selection, like drag-and-drop uploading.
    
    This change completely redesigns it to provide a more self-contained,
    guided experience for providing an avatar. The file input is gone (well,
    hidden), replaced by instructional text with a "browse" link inside.
    The avatar is shrunk down to 48x48px, which is a more appropriate size
    for the UI.
    
    The entire configuration area for setting the avatar is now within a
    dashed box, which functions as a drop area for drag-and-drop operations.
    When dragging over the box, the instructional text changes to let the
    user know they can drop it there to set the text. On drop, we attempt to
    set the file input's list to that of the drop operation. If that fails
    (which will be the case on older browsers and on IE/Edge currently), we
    display a helpful little alert telling them to try browsing instead.
    
    The browse dialog no longer accepts any file type by default. Instead,
    on browsers that support this, we list the mimetypes we're comfortable
    with. This will default the dialog to showing only compatible files.
    
    Testing Done:
    Tested clicking the "browse" text to open the dialog and browse for
    image files.
    
    Tested clicking the avatar icon to open the dialog and browse for
    image files.
    
    Tested dragging-and-dropping image files onto the drop area to set a
    file. Confirmed that on browsers that did not support setting the file
    input, I received an alert and didn't hit any JavaScript errors (and
    the avatar was not updated). Confirmed on browsers that did support
    setting the file input, the image was set and I could upload it.
    
    Tested drag-and-dropping PNGs, JPEGs, GIFs, and other file types to
    check for errors.
    
    Tested drag-and-dropping multiple files at once to check for errors.
    
    Tested saving the form with new avatars using all methods.
    
    All tests were performed on Chrome, Firefox, Safari, IE 10, and Edge.
    
    Reviewed at https://reviews.reviewboard.org/r/9486/

--- djblets/static/djblets/js/avatars/views/fileUploadSettingsFormView.es6.js
+++ djblets/static/djblets/js/avatars/views/fileUploadSettingsFormView.es6.js
@@ -6,16 +6,24 @@ const allowedMimeTypes = [
 ];
 
 
+const ParentView = Djblets.Avatars.ServiceSettingsFormView;
+
+
 /**
  * A file upload avatar settings form.
  *
  * This form provides a preview of the uploaded avatar.
  */
-Djblets.Avatars.FileUploadSettingsFormView = Djblets.Avatars.ServiceSettingsFormView.extend({
+Djblets.Avatars.FileUploadSettingsFormView = ParentView.extend({
     events: {
-        'change #id_file-upload-avatar_upload': '_onFileChanged'
+        'change #id_file-upload-avatar_upload': '_onFileChanged',
+        'click .avatar-file-upload-browse': '_onBrowseClicked',
+        'click .avatar-preview': '_onBrowseClicked',
+        'dragenter .avatar-file-upload-config': '_onDragEnter',
+        'dragover .avatar-file-upload-config': '_onDragOver',
+        'dragleave .avatar-file-upload-config': '_onDragLeave',
+        'drop .avatar-file-upload-config': '_onDrop',
     },
-
 
     /**
      * Validate the form.
@@ -23,15 +31,15 @@ Djblets.Avatars.FileUploadSettingsFormView = Djblets.Avatars.ServiceSettingsForm
      * If a file is selected, ensure it is has the correct MIME type.
      */
     validate() {
-        const file = this.$('#id_file-upload-avatar_upload')[0].files[0];
+        const file = this._$fileInput[0].files[0];
 
         if (!file) {
             alert(gettext('You must choose a file.'));
             return false;
         }
 
-        if (!allowedMimeTypes.some(el => el === file.type)) {
-            alert(gettext('Invalid file format'));
+        if (!allowedMimeTypes.some(el => (el === file.type))) {
+            alert(gettext("This wasn't a valid image file format. Please provide a PNG, JPEG, or GIF file."));
             return false;
         }
 
@@ -46,38 +54,204 @@ Djblets.Avatars.FileUploadSettingsFormView = Djblets.Avatars.ServiceSettingsForm
      *     This view (for chaining).
      */
     render() {
+        this._$box = this.$('.avatar-file-upload-config');
         this._$preview = this.$('.avatar-preview');
+        this._$fileInput = this.$('#id_file-upload-avatar_upload');
 
         return this;
     },
 
     /**
-     * Handle to the selected file being changed.
+     * Handler for a click event on the "browse" instruction text.
+     *
+     * This will trigger opening the hidden file input.
+     *
+     * Args:
+     *     e (jQuery.Event):
+     *         The click event.
+     */
+    _onBrowseClicked(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        /*
+         * Clicking on the file input itself is not reliable. There are ways
+         * to make it work, but the browser actively avoids letting you do it if
+         * it seems to be hidden. However, it works just fine universally to
+         * click on the label.
+         */
+        this.$('#avatar-file-upload-browse-label').click();
+    },
+
+    /**
+     * Handler for a drag enter event.
+     *
+     * If the configuration box is being hovered over, this will enable the
+     * hover state, giving users an indication that they can drop the image
+     * there.
+     *
+     * Args:
+     *     e (jQuery.Event):
+     *         The drag over event.
+     */
+    _onDragEnter(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (e.target === this._$box[0]) {
+            this._$box
+                .width(this._$box.width())
+                .addClass('drag-hover');
+        }
+    },
+
+    /**
+     * Handler for a drag over event.
+     *
+     * If the configuration box is being hovered over, this will set the drop
+     * effect.
+     *
+     * Args:
+     *     e (jQuery.Event):
+     *         The drag over event.
+     */
+    _onDragOver(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (e.target === this._$box[0]) {
+            const dt = e.originalEvent.dataTransfer;
+
+            if (dt) {
+                dt.dropEffect = 'copy';
+            }
+        }
+    },
+
+    /**
+     * Handler for a drag leave event.
+     *
+     * If the configuration box is being left, this will remove the hover state
+     * and reset the drop effect.
+     *
+     * Args:
+     *     e (jQuery.Event):
+     *         The drag leave event.
+     */
+    _onDragLeave(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (e.target === this._$box[0]) {
+            this._$box
+                .removeClass('drag-hover')
+                .width('auto');
+
+            const dt = e.originalEvent.dataTransfer;
+
+            if (dt) {
+                dt.dropEffect = 'none';
+            }
+        }
+    },
+
+    /**
+     * Handler for a drop operation.
+     *
+     * This will remove the hover state and attempt to set the list of files
+     * on the file input. If this fails (which will be the case on some browsers
+     * with older behavior), the user will receive an alert telling them it
+     * failed and to try browsing instead.
+     *
+     * If all goes well, the avatar will be ready for upload and the preview
+     * image will be updated.
+     *
+     * Args:
+     *     e (jQuery.Event):
+     *         The drop event.
+     */
+    _onDrop(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        this._$box.removeClass('drag-hover');
+
+        const dt = e.originalEvent.dataTransfer;
+        const files = dt && dt.files;
+
+        if (!files || files.length === 0) {
+            return;
+        }
+
+        if (files.length > 1) {
+            alert(gettext("You can only set one file as your avatar. Please drag and drop a single file."));
+            return;
+        }
+
+        const fileType = files[0].type;
+
+        if (fileType !== 'image/png' && fileType !== 'image/jpeg' &&
+            fileType !== 'image/gif') {
+            alert(gettext("This doesn't appear to be a compatible image file for avatars. Please upload a PNG, JPEG, or GIF file."));
+            return;
+        }
+
+        try {
+            this._$fileInput[0].files = files;
+        } catch (exc) {
+            /*
+             * While most modern browsers allow setting the `files` property of
+             * an input field to the rest of a drag-and-drop operation, not all
+             * do (I'm looking at you, IE/Edge). Older browsers will also
+             * complain. So instead of outright failing, tell the user that this
+             * won't work and suggest a workaround.
+             */
+            alert(gettext("Looks like dragging to upload a file isn't going to work with your browser. Try browsing for a file instead."));
+            return;
+        }
+
+        this._setAvatarFromFile(files[0]);
+    },
+
+    /**
+     * Handler for when the file input has changed.
      *
      * This will update the preview image.
      *
      * Args:
-     *     e (Event):
+     *     e (jQuery.Event):
      *         The change event.
      */
     _onFileChanged(e) {
         const file = e.target.files[0];
 
         if (file) {
-            const reader = new FileReader();
-            reader.addEventListener('load', () => {
-                this._$preview
-                    .children()
-                    .eq(0)
-                    .replaceWith(
-                        $('<img />')
-                            .attr('src', reader.result)
-                    );
-            });
-
-            reader.readAsDataURL(file);
+            this._setAvatarFromFile(file);
         }
-    }
+    },
+
+    /**
+     * Set the avatar from the provided file upload.
+     *
+     * Args:
+     *     file (File):
+     *         The file that was uploaded.
+     */
+    _setAvatarFromFile(file) {
+        const reader = new FileReader();
+
+        reader.addEventListener('load', () => {
+            this._$preview
+                .empty()
+                .removeClass('avatar-preview-unset')
+                .append($('<img />').attr({
+                     src: reader.result,
+                     alt: gettext('Your new avatar'),
+                 }));
+        });
+
+        reader.readAsDataURL(file);
+    },
 });
 
 
