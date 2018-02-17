43c1b98a564669dde530743d34b161e62663e7a5
Christian Hammond <christian@beanbaginc.com>
2018-02-17 01:01:44 -0800

    Add unit tests for Config Forms, and fix a 404 error.
    This introduces a series of Python unit tests for `ConfigPage`,
    `ConfigPagesView`, and `ConfigPage`, covering the initial states, HTTP
    responses, and basic functionality. These will help catch regressions
    and help with porting to newer versions of Django.
    
    It also fixes a `Http404` that was being returned instead of raised.
    
    Testing Done:
    Unit tests pass.
    
    Reviewed at https://reviews.reviewboard.org/r/9624/

---
+++ djblets/configforms/tests/test_config_page.py
@@ -0,0 +1,109 @@
+"""Unit tests for djblets.configforms.pages.ConfigPage."""
+
+from __future__ import unicode_literals
+
+import re
+
+from django.contrib.auth.models import User
+from django.test.client import RequestFactory
+
+from djblets.configforms.forms import ConfigPageForm
+from djblets.configforms.pages import ConfigPage
+from djblets.configforms.views import ConfigPagesView
+from djblets.testing.testcases import TestCase
+
+
+class TestForm1(ConfigPageForm):
+    form_id = 'my-form-1'
+    form_title = 'Form 1'
+
+
+class TestForm2(ConfigPageForm):
+    form_id = 'my-form-2'
+    form_title = 'Form 2'
+
+
+class TestForm3(ConfigPageForm):
+    form_id = 'my-form-3'
+    form_title = 'Form 3'
+
+    def is_visible(self):
+        return False
+
+
+class TestPage(ConfigPage):
+    page_id = 'my-page'
+    form_classes = [TestForm1, TestForm2, TestForm3]
+
+
+class ConfigPageTests(TestCase):
+    """Unit tests for djblets.configforms.pages.ConfigPage."""
+
+    def setUp(self):
+        super(ConfigPageTests, self).setUp()
+
+        self.request = RequestFactory().request()
+        self.user = User.objects.create_user(username='test-user',
+                                             password='test-user')
+        self.page = TestPage(ConfigPagesView, self.request, self.user)
+
+    def test_initial_state(self):
+        """Testing ConfigPage initial state"""
+        self.assertEqual(len(self.page.forms), 2)
+        self.assertIsInstance(self.page.forms[0], TestForm1)
+        self.assertIsInstance(self.page.forms[1], TestForm2)
+
+    def test_is_visible_with_visible_forms(self):
+        """Testing ConfigPage.is_visible with visible forms"""
+        self.assertTrue(self.page.is_visible())
+
+    def test_is_visible_with_no_visible_forms(self):
+        """Testing ConfigPage.is_visible without visible forms"""
+        class TestPage(ConfigPage):
+            page_id = 'my-page'
+            form_classes = [TestForm3]
+
+        page = TestPage(ConfigPagesView, self.request, self.user)
+        self.assertFalse(page.is_visible())
+
+    def test_render(self):
+        """Testing ConfigPage.render"""
+        # Filter out the CSRF token, since it's hard to match.
+        rendered = re.sub(r"<input.+name='csrfmiddlewaretoken'.*>",
+                          '',
+                          self.page.render())
+
+        self.assertHTMLEqual(
+            rendered,
+            '<div class="box-container">'
+            ' <div class="box">'
+            '  <div class="box-inner">'
+            '   <div class="box-head">'
+            '    <h1 class="box-title">Form 1</h1>'
+            '   </div>'
+            '   <div class="box-main box-foot">'
+            '    <form method="post" action=".#my-page" id="form_my-form-1">'
+            '     <input id="id_form_target" name="form_target"'
+            '            type="hidden" value="my-form-1">'
+            '     <input type="submit" class="btn" value="Save">'
+            '    </form>'
+            '   </div>'
+            '  </div>'
+            ' </div>'
+            '</div>'
+            '<div class="box-container">'
+            ' <div class="box">'
+            '  <div class="box-inner">'
+            '   <div class="box-head">'
+            '    <h1 class="box-title">Form 2</h1>'
+            '   </div>'
+            '   <div class="box-main box-foot">'
+            '    <form method="post" action=".#my-page" id="form_my-form-2">'
+            '     <input id="id_form_target" name="form_target"'
+            '            type="hidden" value="my-form-2">'
+            '     <input type="submit" class="btn" value="Save">'
+            '    </form>'
+            '   </div>'
+            '  </div>'
+            ' </div>'
+            '</div>')
---
+++ djblets/configforms/tests/test_config_pages_view.py
@@ -0,0 +1,161 @@
+"""Unit tests for djblets.configforms.views.ConfigPagesView."""
+
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
+from django.http import Http404
+from django.test.client import RequestFactory
+from django.utils import six
+
+from djblets.configforms.forms import ConfigPageForm
+from djblets.configforms.pages import ConfigPage
+from djblets.configforms.views import ConfigPagesView
+from djblets.testing.testcases import TestCase
+
+
+class TestForm1(ConfigPageForm):
+    form_id = 'my-form-1'
+    form_title = 'Form 1'
+
+    def save(self):
+        pass
+
+
+class TestForm2(ConfigPageForm):
+    form_id = 'my-form-2'
+    form_title = 'Form 2'
+
+
+class TestForm3(ConfigPageForm):
+    form_id = 'my-form-3'
+    form_title = 'Form 3'
+
+    def is_visible(self):
+        return False
+
+
+class TestPage1(ConfigPage):
+    page_id = 'my-page-1'
+    form_classes = [TestForm1]
+
+
+class TestPage2(ConfigPage):
+    page_id = 'my-page-2'
+    form_classes = [TestForm2]
+
+
+class TestPage3(ConfigPage):
+    page_id = 'my-page-3'
+    form_classes = [TestForm3]
+
+
+class MyConfigPagesView(ConfigPagesView):
+    title = 'My Page Title'
+    nav_title = 'My Nav Entry'
+    page_classes = [TestPage1, TestPage2, TestPage3]
+
+    css_bundle_names = ['my-css-bundle']
+    js_bundle_names = ['my-js-bundle']
+
+    js_model_class = 'MyModel'
+    js_view_class = 'MyView'
+
+    def get_js_model_data(self):
+        return {
+            'my-attr': 'value',
+        }
+
+    def get_js_view_data(self):
+        return {
+            'my-option': 'value',
+        }
+
+
+class ConfigPagesViewTests(TestCase):
+    """Unit tests for djblets.configforms.views.ConfigPagesView."""
+
+    def test_dispatch_initial_state(self):
+        """Testing ConfigPagesView.dispatch initial state"""
+        request = RequestFactory().request()
+        request.user = User.objects.create(username='test-user')
+
+        view = MyConfigPagesView()
+        view.request = request
+
+        response = view.dispatch(view.request)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(view.pages), 3)
+        self.assertIsInstance(view.pages[0], TestPage1)
+        self.assertIsInstance(view.pages[1], TestPage2)
+        self.assertIsInstance(view.pages[2], TestPage3)
+        self.assertEqual(set(six.iterkeys(view.forms)),
+                         {'my-form-1', 'my-form-2'})
+
+    def test_get_context_data(self):
+        """Testing ConfigPagesView.get_context_data"""
+        request = RequestFactory().request()
+        request.user = User.objects.create(username='test-user')
+
+        view = MyConfigPagesView()
+        view.request = request
+
+        view.dispatch(view.request)
+
+        self.assertEqual(
+            view.get_context_data(),
+            {
+                'base_template_name': 'base.html',
+                'page_title': 'My Page Title',
+                'nav_title': 'My Nav Entry',
+                'pages_id': 'config_pages',
+                'pages': view.pages,
+                'css_bundle_names': ['my-css-bundle'],
+                'js_bundle_names': ['my-js-bundle'],
+                'js_model_class': 'MyModel',
+                'js_view_class': 'MyView',
+                'js_model_data': {
+                    'my-attr': 'value',
+                },
+                'js_view_data': {
+                    'my-option': 'value',
+                },
+                'forms': list(six.itervalues(view.forms)),
+            })
+
+    def test_post_without_form_target(self):
+        """Testing ConfigPagesView.dispatch with POST and no form_target"""
+        request = RequestFactory().post('/config/')
+        request.user = User.objects.create(username='test-user')
+        request._dont_enforce_csrf_checks = True
+
+        view = MyConfigPagesView.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_with_invalid_form_target(self):
+        """Testing ConfigPagesView.dispatch with POST and invalid form_target
+        """
+        request = RequestFactory().post('/config/', {
+            'form_target': 'bad',
+        })
+        request.user = User.objects.create(username='test-user')
+        request._dont_enforce_csrf_checks = True
+
+        view = MyConfigPagesView.as_view()
+
+        with self.assertRaises(Http404):
+            view(request)
+
+    def test_post_with_success(self):
+        """Testing ConfigPagesView.dispatch with POST and success"""
+        request = RequestFactory().post('/config/', {
+            'form_target': 'my-form-1',
+        })
+        request.user = User.objects.create(username='test-user')
+        request._dont_enforce_csrf_checks = True
+
+        view = MyConfigPagesView.as_view()
+        response = view(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['Location'], '/config/')
