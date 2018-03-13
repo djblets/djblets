2f611e1f6708d73a27d17133d9ff645ce03605be
Christian Hammond <christian@beanbaginc.com>
2018-03-12 22:58:58 -0700

    Add a JavaScript utility function for building URLs.
    This adds a new `Djblets.buildURL()` function that can build a URL from
    a base path, query string data, and an anchor. It takes care of encoding
    query string data and ensuring the resulting URL is formatted just
    right.
    
    Testing Done:
    Unit tests pass.
    
    Reviewed at https://reviews.reviewboard.org/r/9716/

--- djblets/static/djblets/js/utils/tests/urlsTests.es6.js
+++ djblets/static/djblets/js/utils/tests/urlsTests.es6.js
@@ -1,4 +1,127 @@
 suite('djblets/utils/urls', function() {
+    describe('buildURL', function() {
+        it('With just baseURL', function() {
+            const url = Djblets.buildURL({
+                baseURL: 'https://example.com/abc123/',
+            });
+
+            expect(url).toBe('https://example.com/abc123/');
+        });
+
+        describe('With anchor', function() {
+            it('With leading "#"', function() {
+                const url = Djblets.buildURL({
+                    baseURL: 'https://example.com/abc123/',
+                    anchor: '#my-anchor',
+                });
+
+                expect(url).toBe('https://example.com/abc123/#my-anchor');
+            });
+
+            it('Without leading "#"', function() {
+                const url = Djblets.buildURL({
+                    baseURL: 'https://example.com/abc123/',
+                    anchor: 'my-anchor',
+                });
+
+                expect(url).toBe('https://example.com/abc123/#my-anchor');
+            });
+        });
+
+        describe('With queryData', function() {
+            describe('As string', function() {
+                it('With leading "?"', function() {
+                    const url = Djblets.buildURL({
+                        baseURL: 'https://example.com/abc123/',
+                        queryData: '?a=b&c=d',
+                    });
+
+                    expect(url).toBe('https://example.com/abc123/?a=b&c=d');
+                });
+
+                it('Without leading "?"', function() {
+                    const url = Djblets.buildURL({
+                        baseURL: 'https://example.com/abc123/',
+                        queryData: 'a=b&c=d',
+                    });
+
+                    expect(url).toBe('https://example.com/abc123/?a=b&c=d');
+                });
+
+                it('Empty', function() {
+                    const url = Djblets.buildURL({
+                        baseURL: 'https://example.com/abc123/',
+                        queryData: '',
+                    });
+
+                    expect(url).toBe('https://example.com/abc123/');
+                });
+            });
+
+            describe('As object', function() {
+                it('With value', function() {
+                    const url = Djblets.buildURL({
+                        baseURL: 'https://example.com/abc123/',
+                        queryData: {
+                            a: 'b',
+                        },
+                    });
+
+                    expect(url).toBe('https://example.com/abc123/?a=b');
+                });
+
+                it('Empty', function() {
+                    const url = Djblets.buildURL({
+                        baseURL: 'https://example.com/abc123/',
+                        queryData: {},
+                    });
+
+                    expect(url).toBe('https://example.com/abc123/');
+                });
+            });
+
+            describe('As array', function() {
+                it('With value', function() {
+                    const url = Djblets.buildURL({
+                        baseURL: 'https://example.com/abc123/',
+                        queryData: [
+                            {
+                                name: 'a',
+                                value: 'b',
+                            },
+                            {
+                                name: 'c',
+                                value: 'd',
+                            },
+                        ],
+                    });
+
+                    expect(url).toBe('https://example.com/abc123/?a=b&c=d');
+                });
+
+                it('Empty', function() {
+                    const url = Djblets.buildURL({
+                        baseURL: 'https://example.com/abc123/',
+                        queryData: [],
+                    });
+
+                    expect(url).toBe('https://example.com/abc123/');
+                });
+            });
+        });
+
+        it('With all options', function() {
+            const url = Djblets.buildURL({
+                baseURL: 'https://example.com/abc123/',
+                queryData: 'a=b&c=d',
+                anchor: 'my-anchor',
+            });
+
+            expect(url).toBe(
+                'https://example.com/abc123/?a=b&c=d#my-anchor');
+        });
+    });
+
     describe('parseQueryString', function() {
         it('Empty query string', function() {
             expect(Djblets.parseQueryString('')).toEqual({});
