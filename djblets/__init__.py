60f13f5d9c98a0867d85402500a746c814a27c4d
Christian Hammond <christian@beanbaginc.com>
2019-06-11 22:29:18 -0700

    Bump to 1.0.13 dev.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (1, 0, 12, 'final', 0, True)
+VERSION = (1, 0, 13, 'alpha', 0, False)
 
 
 def get_version_string():
