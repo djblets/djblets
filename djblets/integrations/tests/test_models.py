1e167ab27a8decd4b0d8643a1d254e337237b6aa
Christian Hammond <christian@beanbaginc.com>
2020-05-08 16:58:19 -0700

    Add an improved string representation for IntegrationConfig.
    The `IntegrationConfig` string representation wasn't very nice. It
    didn't list the configuration name, and it used the integration ID
    directly. Not exactly friendly.
    
    This improves it to return a string in the form of "Name (Integration)",
    which is a bit more useful and friendly all-around.
    
    Testing Done:
    Checked this in the admin UI in places where it showed up.
    
    Tested with configurations that mapped to an available integration
    and ones that mapped to an unavailable integration (making sure that the
    integration ID fallback worked).
    
    Unit tests pass.
    
    Reviewed at https://reviews.reviewboard.org/r/11015/

--- djblets/integrations/tests/test_models.py
+++ djblets/integrations/tests/test_models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+from django.utils import six
+
 from djblets.integrations.integration import Integration
 from djblets.integrations.manager import IntegrationManager
 from djblets.integrations.tests.models import IntegrationConfig
@@ -7,6 +9,8 @@ from djblets.integrations.tests.testcases import IntegrationsTestCase
 
 
 class DummyIntegration1(Integration):
+    name = 'Dummy'
+
     default_settings = {
         'foo': 'default-foo',
     }
@@ -24,7 +28,7 @@ class IntegrationConfigTests(IntegrationsTestCase):
         self.manager = IntegrationManager(IntegrationConfig)
         self.integration = \
             self.manager.register_integration_class(DummyIntegration1)
-        self.config = self.integration.create_config()
+        self.config = self.integration.create_config(name='Test Config')
         self.config.manager = self.manager
 
     def test_integration(self):
@@ -49,3 +53,15 @@ class IntegrationConfigTests(IntegrationsTestCase):
         """Testing BaseIntegrationConfig.set"""
         self.config.set('my-key', 'my-value')
         self.assertEqual(self.config.settings.get('my-key'), 'my-value')
+
+    def test_str_with_available_integration(self):
+        """Testing BaseIntegrationConfig.__str__ with available integration"""
+        self.assertEqual(six.text_type(self.config), 'Test Config (Dummy)')
+
+    def test_str_without_available_integration(self):
+        """Testing BaseIntegrationConfig.__str__ without available integration
+        """
+        self.config.integration_id = 'foo.bar.BadIntegration'
+
+        self.assertEqual(six.text_type(self.config),
+                         'Test Config (foo.bar.BadIntegration)')
