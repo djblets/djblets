400c508d04a39d4bda12cc3a1eb513018b838e53
Christian Hammond <christian@beanbaginc.com>
2021-05-26 16:41:42 -0700

    Branch for Djblets 3.x development.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -33,7 +33,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (2, 2, 2, 'alpha', 0, False)
+VERSION = (3, 0, 0, 'alpha', 0, False)
 
 
 def get_version_string():
