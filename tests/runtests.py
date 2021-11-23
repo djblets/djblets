5edd3efad750be0fc75f8bd8d13ec6a55445f0cc
David Trowbridge <trowbrds@gmail.com>
2021-11-23 10:59:17 -0700

    Add compat imports for collections.abc.
    Python 3.10 removed a bunch of deprecated aliases for items in the
    collections.abc module. Unfortunately for us, both Django 1.11 and nose
    rely on these imports. This change adds a compat module that will
    recreate the aliases we need.
    
    Testing Done:
    Ran unit tests with Python 3.10.
    
    Reviewed at https://reviews.reviewboard.org/r/11881/

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
-import nose
+
 import os
 import shutil
 import stat
 import sys
 import warnings
+
+# This has to happen before we import nose.
+import djblets.util.compat.python.collections
+
+import nose
 
 
 def run_tests(verbosity=1, interactive=False):
