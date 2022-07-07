18c7a7b51126a9bd4a5947da0be1062edcf40363
Christian Hammond <christian@beanbaginc.com>
2022-07-06 17:51:50 -0700

    Release Djblets 3.0 beta 1.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -12,7 +12,7 @@ import djblets.util.compat.python.collections
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (3, 0, 0, 'alpha', 0, False)
+VERSION = (3, 0, 0, 'beta', 1, True)
 
 
 def get_version_string():
