c7054a495b95c069d630054e8185e8bec691b39a
Christian Hammond <christian@beanbaginc.com>
2023-01-08 22:18:33 -0800

    Improve caching and invalidation for URI templates on the root resource.
    The root resource attempts to cache the generation of URI templates, to
    make access to the resource as speedy as possible. However, there are
    two problems, which some installs may encounter depending on their use:
    
    * The URI template cache can grow unbounded. This is only an issue if
      the root resource is namespaced to, for instance, a team/organization
      name.
    
    * Template caching/clearing/generation is not thread-safe. The cache is
      shared across all threads, but is written to/cleared with no regard to
      other threads, which could *in theory* pose problems.
    
    This addresses both issues through a rewrite of the caching. There's now
    a maximum of 50 cached entries. This is a LRU cache. When hitting the
    limit, the least-recently used item will be removed from the cache.
    
    It also locks on any cache manipulation. This includes building of URI
    templates. When looking up an item not found in the cache, the URI
    templates will be built within that same lock. This should avoid more
    than one thread ever building the URI templates at a time, and should
    ensure every other thread benefits from the cached entries.
    
    Subclasses that need to augment the list of URI templates are now
    encouraged to override the new `build_uri_templates()` method instead of
    `get_uri_templates()` in order to take advantage of the caching behavior
    (though legacy code and behavior will not break).
    
    Testing Done:
    Unit tests passed in Djblets and Review Board.
    
    Reviewed at https://reviews.reviewboard.org/r/12746/

--- djblets/webapi/resources/root.py
+++ djblets/webapi/resources/root.py
@@ -2,12 +2,15 @@
 
 import logging
 from collections import defaultdict, namedtuple
+from threading import RLock
+from typing import Callable, Dict, Optional, OrderedDict
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.http import HttpResponseNotModified
+from django.http import HttpRequest, HttpResponseNotModified
 from django.utils.translation import gettext_lazy as _
 from django.urls import path
+from typing_extensions import TypeAlias
 
 from djblets.urls.patterns import never_cache_patterns
 from djblets.webapi.errors import DOES_NOT_EXIST
@@ -16,6 +19,136 @@ from djblets.webapi.responses import WebAPIResponseError
 
 
 logger = logging.getLogger(__name__)
+
+
+#: A type alias for a mapping of URI templates.
+#:
+#: Each key is a URI template name, and each value is a URL.
+#:
+#: Version Added:
+#:     3.2
+URITemplates: TypeAlias = Dict[str, str]
+
+
+#: A mapping of API resource instances to URI templates.
+#:
+#: Version Added:
+#:     3.2
+_URITemplatesByResourceMap: TypeAlias = Dict[Optional[WebAPIResource],
+                                             URITemplates]
+
+
+class _URITemplatesCache:
+    """A least-recently-used cache for URI templates.
+
+    This will map base API URLs to generated dictionaries of URI templates. It
+    holds up to a maximum number of cached entries, and will discard the oldest
+    as needed once the limit is reached.
+
+    Since cache state is shared between threads, adding and fetching items are
+    done behind a thread lock, to avoid issues with multiple threads trying to
+    manipulate the cache at the same time.
+
+    Version Added:
+        3.2
+    """
+
+    ######################
+    # Instance variables #
+    ######################
+
+    #: The maximum number of URI templates to store in the cache.
+    #:
+    #: Type:
+    #:     int
+    max_size: int
+
+    def __init__(
+        self,
+        max_size: int = 50,
+    ) -> None:
+        """Initialize the cache.
+
+        Args:
+            max_size (int, optional):
+                The maximum size of the cache.
+        """
+        self.max_size = max_size
+        self._cache = OrderedDict[str, URITemplates]()
+        self._lock = RLock()
+
+    def add(
+        self,
+        base_href: str,
+        uri_templates: URITemplates,
+    ) -> None:
+        """Add URI templates to the cache.
+
+        If the cache is full, the last item will be discarded.
+
+        Args:
+            base_href (str):
+                The base API URL mapped to the URI templates.
+
+            uri_templates (dict):
+                The dictionary of generated URI templates.
+        """
+        with self._lock:
+            cache = self._cache
+
+            if len(cache) >= self.max_size:
+                cache.popitem(last=False)
+
+            cache[base_href] = uri_templates
+
+    def get(
+        self,
+        base_href: str,
+        *,
+        build_func: Optional[Callable[[str], URITemplates]] = None,
+    ) -> URITemplates:
+        """Return URI templates from the cache.
+
+        If the URI templates are in the cache, they'll be returned and then
+        marked as most-recently used, to avoid falling out of cache too soon.
+
+        Args:
+            base_href (str):
+                The base API URL mapped to the URI templates.
+
+            build_func (callable, optional):
+                Optional function to call to build the results if missing.
+
+                This will be built while a lock is held.
+
+        Returns:
+            dict:
+            The URI templates found in the cache. This may be
+            newly-constructed.
+
+        Raises:
+            KeyError:
+                No cached content exists for the given ``base_href``.
+        """
+        with self._lock:
+            cache = self._cache
+
+            try:
+                uri_templates = cache[base_href]
+                cache.move_to_end(base_href)
+            except KeyError:
+                if build_func is None:
+                    raise
+
+                uri_templates = build_func(base_href)
+                self.add(base_href, uri_templates)
+
+            return uri_templates
+
+    def clear(self) -> None:
+        """Clear the URI template cache."""
+        with self._lock:
+            self._cache.clear()
 
 
 class RootResource(WebAPIResource):
@@ -56,8 +189,9 @@ class RootResource(WebAPIResource):
     def __init__(self, child_resources=[], include_uri_templates=True):
         super(RootResource, self).__init__()
         self.list_child_resources = child_resources
-        self._uri_templates = {}
-        self._registered_uri_templates = {}
+
+        self._cached_uri_templates = _URITemplatesCache()
+        self._registered_uri_templates: _URITemplatesByResourceMap = {}
         self._include_uri_templates = include_uri_templates
 
     def get_etag(self, request, obj, *args, **kwargs):
@@ -93,7 +227,12 @@ class RootResource(WebAPIResource):
 
         return data
 
-    def get_uri_templates(self, request, *args, **kwargs):
+    def get_uri_templates(
+        self,
+        request: HttpRequest,
+        *args,
+        **kwargs,
+    ) -> URITemplates:
         """Return all URI templates in the resource tree.
 
         REST APIs can be very chatty if a client wants to be well-behaved
@@ -119,68 +258,89 @@ class RootResource(WebAPIResource):
 
         Returns:
             dict:
-                A mapping of resources to their URI templates.
+            A mapping of resources to their URI templates.
         """
-        if not self._uri_templates:
-            self._uri_templates = {}
-
         base_href = request.build_absolute_uri()
 
-        if base_href not in self._uri_templates:
-            templates = defaultdict(list)
-            unassigned_templates = self._registered_uri_templates.get(
-                None, {})
+        return self._cached_uri_templates.get(
+            base_href,
+            build_func=self.build_uri_templates)
 
-            for uri_template_name, href in unassigned_templates.items():
-                templates[uri_template_name].append(href)
+    def build_uri_templates(
+        self,
+        base_href: str,
+    ) -> URITemplates:
+        """Build the URI templates to include in the payload.
 
-            for entry in self.walk_resources(self, base_href):
-                uri_template_name = entry.uri_template_name
+        The resource will cache these automatically in local memory, so that
+        repeated calls will often be unnecessary. This allows for things like
+        per-organization APIs that each have their own cache.
 
-                if uri_template_name is None:
-                    continue
+        Caches are automatically invalidated as needed.
 
-                templates[uri_template_name].append(entry.list_href)
+        Version Added:
+            3.2
 
-                if entry.is_list:
-                    list_templates = self._registered_uri_templates.get(
-                        entry.resource, {})
+        Args:
+            base_href (str):
+                The base URL for the API endpoint.
 
-                    for uri_template_name, href in list_templates.items():
-                        templates[uri_template_name].append(
-                            '%s%s' % (entry.list_href, href))
+        Returns:
+            dict:
+            A dictionary mapping URI template keys to template values.
+        """
+        templates = defaultdict(list)
+        unassigned_templates = self._registered_uri_templates.get(
+            None, {})
 
-            final_templates = {}
+        for uri_template_name, href in unassigned_templates.items():
+            templates[uri_template_name].append(href)
 
-            for uri_template_name, uri_template in templates.items():
-                if len(uri_template) > 1:
-                    if settings.DEBUG:
-                        raise ImproperlyConfigured(
-                            _('More than one URI template was mapped to '
-                              'the "%(name)s" name: %(templates)s. Each URI '
-                              'template must be mapped to a unique URI '
-                              'template name in order to be included in the '
-                              'URI templates list. This can be set through '
-                              'the uri_template_name property.')
-                            % {
-                                'name': uri_template_name,
-                                'templates': ', '.join(uri_template)
-                            })
-                    else:
-                        logger.error(
-                            'More than one URI template was mapped to '
-                            'the "%s" name: %s. Only the first one will be '
-                            'included in the URI templates list. To '
-                            'include the other URI templates, they must '
-                            'be mapped to a unique name by setting each '
-                            'resource\'s uri_template_name property.'
-                            % (uri_template_name, ', '.join(uri_template)))
+        for entry in self.walk_resources(self, base_href):
+            uri_template_name = entry.uri_template_name
 
-                final_templates[uri_template_name] = uri_template[0]
+            if uri_template_name is None:
+                continue
 
-            self._uri_templates[base_href] = final_templates
+            templates[uri_template_name].append(entry.list_href)
 
-        return self._uri_templates[base_href]
+            if entry.is_list:
+                list_templates = self._registered_uri_templates.get(
+                    entry.resource, {})
+
+                for uri_template_name, href in list_templates.items():
+                    templates[uri_template_name].append(
+                        '%s%s' % (entry.list_href, href))
+
+        final_templates = {}
+
+        for uri_template_name, uri_template in templates.items():
+            if len(uri_template) > 1:
+                if settings.DEBUG:
+                    raise ImproperlyConfigured(
+                        _('More than one URI template was mapped to '
+                          'the "%(name)s" name: %(templates)s. Each URI '
+                          'template must be mapped to a unique URI '
+                          'template name in order to be included in the '
+                          'URI templates list. This can be set through '
+                          'the uri_template_name property.')
+                        % {
+                            'name': uri_template_name,
+                            'templates': ', '.join(uri_template)
+                        })
+                else:
+                    logger.error(
+                        'More than one URI template was mapped to '
+                        'the "%s" name: %s. Only the first one will be '
+                        'included in the URI templates list. To '
+                        'include the other URI templates, they must '
+                        'be mapped to a unique name by setting each '
+                        'resource\'s uri_template_name property.'
+                        % (uri_template_name, ', '.join(uri_template)))
+
+            final_templates[uri_template_name] = uri_template[0]
+
+        return final_templates
 
     @classmethod
     def walk_resources(cls, resource, list_href):
@@ -249,27 +409,28 @@ class RootResource(WebAPIResource):
 
         return urlpatterns
 
-    def register_uri_template(self, name, relative_path,
-                              relative_resource=None):
+    def register_uri_template(
+        self,
+        name: str,
+        relative_path: str,
+        relative_resource: Optional[WebAPIResource] = None,
+    ) -> None:
         """Register the specified resource for URI template serialization.
 
         This adds the specified name and relative resource to the Root
         Resource's URI templates.
 
         Args:
-            name (unicode):
+            name (str):
                 The name of the associated resource being added to templates.
 
-            relative_path (unicode):
+            relative_path (str):
                 The path of the API resource relative to its parent resources.
 
-            relative_resource (djblets.extensions.resources.ExtensionResource,
+            relative_resource (djblets.webapi.resources.base.WebAPIResource,
                                optional):
                 The resource instance associated with this URI template.
         """
-        # Clear the cache so that new lookups can detect newly added templates.
-        self._uri_templates = {}
-
         templates = self._registered_uri_templates.setdefault(
             relative_resource, {})
 
@@ -283,24 +444,32 @@ class RootResource(WebAPIResource):
         # Allow the URI template to be overwritten if it already exists.
         templates[name] = relative_path
 
-    def unregister_uri_template(self, name, relative_resource=None):
+        # Clear the cache so that new lookups can detect newly added templates.
+        self._cached_uri_templates.clear()
+
+    def unregister_uri_template(
+        self,
+        name: str,
+        relative_resource: Optional[WebAPIResource] = None,
+    ) -> None:
         """Unregister the specified resource for URI template serialization.
 
         This removes the specified name and relative resource to the Root
         Resource's URI templates.
 
         Args:
-            name (unicode):
+            name (str):
                 The name of the resource being removed from templates.
 
-            relative_resource (djblets.extensions.resources.ExtensionResource,
+            relative_resource (djblets.webapi.resources.base.WebAPIResource,
                                optional):
                 The resource instance associated with this URI template.
         """
-        # Clear the cache so that new lookups can detect newly added templates.
-        self._uri_templates = {}
-
         try:
             del self._registered_uri_templates[relative_resource][name]
         except KeyError:
-            pass
+            return
+
+        # Clear the cache so that new lookups won't include the old URI
+        # templates.
+        self._cached_uri_templates.clear()
