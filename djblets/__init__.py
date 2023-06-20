1610ef290ad0bdbeabe93d587af5701524eafdec
Christian Hammond <christian@beanbaginc.com>
2023-06-19 23:20:43 -0700

    Bump to 3.3.1 dev.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -12,7 +12,7 @@ import djblets.util.compat.python.collections
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (3, 3, 0, 'final', 0, True)
+VERSION = (3, 3, 1, 'alpha', 0, False)
 
 
 def get_version_string():
