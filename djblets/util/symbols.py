0b0f0de79888dc31291483bb6bfe2d3a40f0004e
Christian Hammond <christian@beanbaginc.com>
2023-08-10 12:37:12 -0700

    Improve typing support for the UNSET symbol.
    This reworks the internals of the UNSET symbol (which was introduced in
    Djblets 3.3) and makes it easier to integrate it with typing logic and
    type checkers.
    
    `UnsetSymbol` is now an `Enum`, allowing `UNSET` to be defined as a
    `Literal`. This works along with a new `Unsettable[_T]` type alias to
    mark whether a parameter or variable allows `UNSET` to be set.
    
    By making this a `Literal`, type checkers can type narrow when a code
    compares a value to `UNSET`, avoiding any type errors that still wanted
    to consider `UnsetSymbol` a possible value.
    
    Testing Done:
    Made use of this in some in-progress code. Verified that mypy and pyright
    successfully narrowed the types when comparing against `UNSET`.
    
    Reviewed at https://reviews.reviewboard.org/r/13191/

--- djblets/util/symbols.py
+++ djblets/util/symbols.py
@@ -4,20 +4,52 @@ Version Added:
     3.3
 """
 
+from enum import Enum
+from typing import TypeVar, Union
 
-class UnsetSymbol:
-    """A symbol indicating an unset value.
+from typing_extensions import Final, Literal, TypeAlias
+
+
+_T = TypeVar('_T')
+
+
+class UnsetSymbol(Enum):
+    """A type indicating an unsettable value.
 
     This can be useful in functions that take default values to distinguish
     between a value not provided and a false/``None`` value.
 
+    Version Changed:
+        4.0:
+        This is now an Enum, allowing values to be used in
+        :py:class:`typing.Literal`.
+
     Version Added:
         3.3
     """
+
+    UNSET = '<UNSET>'
 
 
 #: An instance of a symbol indicating an unset value.
 #:
 #: Version Added:
 #:     3.3
-UNSET = UnsetSymbol()
+UNSET: Final[Literal[UnsetSymbol.UNSET]] = UnsetSymbol.UNSET
+
+
+#: A generic type alias for marking a type as unsettable.
+#:
+#: This allows for usage like:
+#:
+#: .. code-block:: python
+#:
+#:    def __init__(
+#:        self,
+#:        value: Unsettable[str],
+#:    ) -> None:
+#:        ...
+#:
+#: Version Added:
+#:     4.0
+Unsettable: TypeAlias = Union[Literal[UnsetSymbol.UNSET], _T]
