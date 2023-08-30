15672ff47eb49e4e0c98c36beb65bbd779a5c626
David Trowbridge <trowbrds@gmail.com>
2023-08-30 15:38:18 -0600

    Add a health check view.
    This change adds a view implementation that can be used to set up a
    health check. It simply checks that the database and cache server are
    available, and returns 200 if so and 500 if not (while logging the
    error).
    
    Testing Done:
    Manually set things up to have either the database or cache server fail
    and saw that this view returned the correct results.
    
    Reviewed at https://reviews.reviewboard.org/r/13252/

--- djblets/util/views.py
+++ djblets/util/views.py
@@ -1,18 +1,55 @@
-from django.utils.translation import get_language
-from django.views.i18n import JavaScriptCatalog
+"""Various utility views."""
 
-from djblets.cache.backend import cache_memoize
+from __future__ import annotations
+
+import logging
+from enum import Enum
+from typing import Dict, Sequence
+
+from django.conf import settings
+from django.core.cache import caches
+from django.db import connections
+from django.db.utils import OperationalError
+from django.http import HttpRequest, HttpResponseForbidden, JsonResponse
+from django.http.response import HttpResponseBase
+from django.utils.translation import get_language
+from django.views.generic import View
+from django.views.i18n import JavaScriptCatalog
+from typing_extensions import TypedDict
+
+from djblets.cache.backend import cache_memoize, make_cache_key
 from djblets.cache.serials import generate_locale_serial
 
 
-locale_serials = {}
+logger = logging.getLogger(__name__)
+locale_serials: Dict[str, int] = {}
 
 
-def cached_javascript_catalog(request, domain='djangojs', packages=None):
-    """A cached version of javascript_catalog."""
+def cached_javascript_catalog(
+    request: HttpRequest,
+    domain: str = 'djangojs',
+    packages: Sequence[str] = [],
+) -> HttpResponseBase:
+    """A cached version of javascript_catalog.
+
+    Args:
+        request (django.http.HttpRequest):
+            The HTTP request.
+
+        domain (str):
+            The translation domain to use when looking up strings.
+
+        packages (list of str):
+            The package names to get strings from.
+
+    Returns:
+        django.http.HttpResponse:
+        The response to send back to the client.
+    """
     global locale_serials
 
     package_str = '_'.join(packages)
+
     try:
         serial = locale_serials[package_str]
     except KeyError:
@@ -25,3 +62,149 @@ def cached_javascript_catalog(request, domain='djangojs', packages=None):
                                           packages=packages)(request),
         large_data=True,
         compress_large_data=True)
+
+
+class HealthCheckStatus(str, Enum):
+    """A status indicator for health checks.
+
+    Version Added:
+        4.0
+    """
+    # TODO: this can inherit from StrEnum once we're Python 3.11+
+
+    #: The state when a service is running and can be connected to.
+    UP = 'UP'
+
+    #: The state when a service is down or otherwise cannot be connected to.
+    DOWN = 'DOWN'
+
+    #: The state when it is unknown if a service is up or down.
+    UNKNOWN = 'UNKNOWN'
+
+    def __str__(self) -> str:
+        """Return a string representation of the status.
+
+        This exists so we can compare a string against an enum. Once we move to
+        StrEnum this can be removed.
+
+        Returns:
+            str:
+            The string status.
+        """
+        return str.__str__(self)
+
+
+class HealthCheckResult(TypedDict):
+    """Result structure for health checks.
+
+    Version Added:
+        4.0
+    """
+
+    #: The individual checks that were run.
+    checks: Dict[str, HealthCheckStatus]
+
+    #: A mapping of the check name to the error if the service is down.
+    errors: Dict[str, str]
+
+    #: The overall health status of the server.
+    status: HealthCheckStatus
+
+
+class HealthCheckView(View):
+    """A view for health checks.
+
+    This will check the status of connected database and cache servers, and
+    report whether everything can be accessed. It will return either HTTP 200
+    or 503, and the payload will be a JSON blob defined by
+    :py:class:`HealthCheckResult`.
+
+    Version Added:
+        4.0
+    """
+
+    def get(
+        self,
+        request: HttpRequest,
+        *args,
+        **kwargs,
+    ) -> HttpResponseBase:
+        """Perform a health check.
+
+        This will do a health check on whether the database and cache server
+        can be used. If both are accessible, this will return an HTTP 200. If
+        not, this will return HTTP 500.
+
+        Args:
+            request (django.http.HttpRequest):
+                The HTTP request.
+
+            *args (tuple, unused):
+                Positional arguments, for future expansion.
+
+            **kwargs (dict, unused):
+                Keyword arguments, for future expansion.
+
+        Returns:
+            django.http.JsonResponse:
+            The response to send back to the client.
+        """
+        allowed_ips = getattr(settings, 'DJBLETS_HEALTHCHECK_IPS',
+                              settings.INTERNAL_IPS)
+
+        if request.META.get('REMOTE_ADDR') not in allowed_ips:
+            return HttpResponseForbidden()
+
+        result: HealthCheckResult = {
+            'checks': {},
+            'errors': {},
+            'status': HealthCheckStatus.UP,
+        }
+        status: int = 200
+        success: bool = True
+
+        for key in settings.DATABASES.keys():
+            result_key = f'database.{key}'
+
+            try:
+                db = connections[key]
+
+                with db.cursor():
+                    pass
+
+                result['checks'][result_key] = HealthCheckStatus.UP
+            except OperationalError as e:
+                logger.error('Health check: unable to connect to database '
+                             '"%s": %s',
+                             key, e,
+                             extra={'request': request})
+
+                success = False
+                result['checks'][result_key] = HealthCheckStatus.DOWN
+                result['errors'][result_key] = str(e)
+
+        for key in settings.CACHES.keys():
+            result_key = f'cache.{key}'
+
+            try:
+                cache = caches[key]
+
+                if not cache.get_or_set(make_cache_key('healthcheck'), True):
+                    raise Exception('Unable to communicate with cache server')
+
+                result['checks'][result_key] = HealthCheckStatus.UP
+            except Exception as e:
+                logger.error('Health check: unable to connect to cache '
+                             '"%s": %s',
+                             key, e,
+                             extra={'request': request})
+
+                success = False
+                result['checks'][result_key] = HealthCheckStatus.DOWN
+                result['errors'][result_key] = str(e)
+
+        if not success:
+            status = 503
+            result['status'] = HealthCheckStatus.DOWN
+
+        return JsonResponse(result, status=status)
