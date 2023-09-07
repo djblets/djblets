a765c201593493037b2b4915f0cdee30c3b7406b
Christian Hammond <christian@beanbaginc.com>
2023-09-07 15:58:54 -0700

    Added type hints and modernize docs for djblets.webapi.errors.
    This updates `djblets.webapi.errors` to include type hints and modern
    documentation. This class is ancient, and was not up to our current
    standards.
    
    This is in preparation for upcoming work on error support.
    
    There is one small feature addition in that `with_override()` and
    `with_message()` will now do the right thing if called on a `WebAPIError`
    subclass. While not a pattern we currently explicitly support, this is
    more correct and helps future-proof the class.
    
    Testing Done:
    Unit tests pass.
    
    Reviewed at https://reviews.reviewboard.org/r/13258/

--- djblets/webapi/errors.py
+++ djblets/webapi/errors.py
@@ -1,35 +1,155 @@
 """Error classes and codes for WebAPI."""
 
+from __future__ import annotations
 
-class WebAPIError(object):
+from typing import Callable, Dict, List, Optional, TYPE_CHECKING, Union
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest
+    from djblets.webapi.responses import WebAPIResponseHeaders
+    from typing_extensions import Self, TypeAlias
+
+    #: A mapping of HTTP headers to values, or a callable returning headers.
+    #:
+    #: Version Added:
+    #:     4.0
+    _HTTPHeadersOrCallable: TypeAlias = Union[
+        WebAPIResponseHeaders,
+        Callable[[HttpRequest], WebAPIResponseHeaders]
+    ]
+
+
+class WebAPIError:
+    """An API error, containing an error code and human readable message.
+
+    Version Changed:
+        4.0:
+        * Added Python type hints.
     """
-    An API error, containing an error code and human readable message.
-    """
-    def __init__(self, code, msg, http_status=400, headers={}):
+
+    ######################
+    # Instance variables #
+    ######################
+
+    #: The numeric code for the error.
+    #:
+    #: Type:
+    #:     int
+    code: int
+
+    #: Extra HTTP headers included in the error's HTTP response.
+    #:
+    #: This may also be a callable that returns HTTP headers, given an HTTP
+    #: request.
+    #:
+    #: Type:
+    #:     dict or callable
+    headers: _HTTPHeadersOrCallable
+
+    #: The HTTP status code of the error.
+    #:
+    #: Type:
+    #:     int
+    http_status: int
+
+    #: The error message shown to the caller.
+    #:
+    #: Type:
+    #:     str
+    msg: str
+
+    def __init__(
+        self,
+        code: int,
+        msg: str,
+        http_status: int = 400,
+        headers: _HTTPHeadersOrCallable = {},
+    ) -> None:
+        """Initialize the error.
+
+        Args:
+            code (int):
+                The numeric code for the error.
+
+            msg (str):
+                The error message shown to the caller.
+
+            http_status (int, optional):
+                The HTTP status code for the error.
+
+                This defaults to :http:`400`.
+
+            headers (dict or callable, optional):
+                Extra HTTP headers to include in the error's HTTP response.
+
+                This may also be a callable that returns HTTP headers, given
+                an HTTP request.
+        """
         self.code = code
         self.msg = msg
         self.http_status = http_status
         self.headers = headers
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """Return a string representation of the error.
+
+        Returns:
+            str:
+            The string representation.
+        """
         return '<API Error %d, HTTP %d: %s>' % (self.code, self.http_status,
                                                 self.msg)
 
-    def with_overrides(self, msg=None, headers=None):
-        """Overrides the default message and/or headers for an error."""
+    def with_overrides(
+        self,
+        msg: Optional[str] = None,
+        headers: Optional[_HTTPHeadersOrCallable] = None,
+    ) -> Self:
+        """Return an error with overridden values.
+
+        The returned error will be based on this error, but with a custom
+        message or headers.
+
+        Args:
+            msg (str, optional):
+                The custom error message.
+
+            headers (dict or callable, optional):
+                Custom HTTP headers to include in the error's HTTP response.
+
+                This may also be a callable that returns HTTP headers, given
+                an HTTP request.
+
+                These will override the default HTTP headers for the error.
+
+        Returns:
+            WebAPIError:
+            The new error instance.
+        """
         if headers is None:
             headers = self.headers
 
-        return WebAPIError(self.code, msg or self.msg, self.http_status,
-                           headers)
+        return type(self)(code=self.code,
+                          msg=msg or self.msg,
+                          http_status=self.http_status,
+                          headers=headers)
 
-    def with_message(self, msg):
-        """
-        Overrides the default message for a WebAPIError with something
-        more context specific.
+    def with_message(
+        self,
+        msg: str,
+    ) -> Self:
+        """Return an error with an overridden error message.
 
-        Example:
-        return ENABLE_EXTENSION_FAILED.with_message('some error message')
+        The returned error will be based on this error, but with a custom
+        message.
+
+        Args:
+            msg (str):
+                The custom error message.
+
+        Returns:
+            WebAPIError:
+            The new error instance.
         """
         return self.with_overrides(msg)
 
@@ -38,11 +158,29 @@ class WebAPITokenGenerationError(Exception):
     """An error generating a Web API token."""
 
 
-def _get_auth_headers(request):
+def _get_auth_headers(
+    request: HttpRequest,
+) -> WebAPIResponseHeaders:
+    """Return HTTP headers for an authentication error.
+
+    The result will include any headers from the authentication backend,
+    along with a :mailheader:`WWW-Authenticate` header containing a
+    comma-separated list of schemes set by auth backends (see
+    :py:attr:`WebAPIAuthBackend.www_auth_scheme
+    <djblets.webapi.auth.backends.base.WebAPIAuthBackend.www_auth_scheme>`.
+
+    Args:
+        request (django.http.HttpRequest):
+            The HTTP request from the client.
+
+    Returns:
+        dict:
+        The mapping of HTTP authentication headers to values.
+    """
     from djblets.webapi.auth.backends import get_auth_backends
 
-    headers = {}
-    www_auth_schemes = []
+    headers: Dict[str, str] = {}
+    www_auth_schemes: List[str] = []
 
     for auth_backend_cls in get_auth_backends():
         auth_backend = auth_backend_cls()
