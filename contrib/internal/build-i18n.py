f2d24d13a49d54e06652e7b6d20c46f857dab561
Christian Hammond <christian@beanbaginc.com>
2024-04-01 19:23:01 -0700

    Remove legacy code from our media and i18n build scripts.
    Our `build-i18n.py` and `build-media.py` scripts were both built to be
    compatible with Python 2 and Django 1.6. We haven't needed this support
    in a while, so we can nuke the shims, conditionals, and legacy future
    imports in the code.
    
    Testing Done:
    Successfully build static media and localization files.
    
    Reviewed at https://reviews.reviewboard.org/r/13649/

--- contrib/internal/build-i18n.py
+++ contrib/internal/build-i18n.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python3
 
-from __future__ import unicode_literals
-
 import os
 import sys
 
-# This must be called before we import from Django, to ensure collections
-# patching works.
-import djblets
-
 import django
 from django.core.management import call_command
+
+import djblets
 
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djblets.settings')
 
-    if hasattr(django, 'setup'):
-        # Django >= 1.7
-        django.setup()
+    django.setup()
 
     os.chdir(os.path.dirname(djblets.__file__))
 
--- contrib/internal/build-media.py
+++ contrib/internal/build-media.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from __future__ import unicode_literals
-
 import os
 import sys
 
@@ -13,14 +11,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(scripts_dir, '..', '..')))
 # Script config directory
 sys.path.insert(0, os.path.join(scripts_dir, 'conf'))
 
-from djblets.dependencies import django_version
-
-import __main__
-__main__.__requires__ = ['Django' + django_version]
-
-# This is required to re-import.
-import pkg_resources
-
 import django
 from django.core.management import call_command
 
@@ -29,9 +19,7 @@ if __name__ == '__main__':
     os.putenv('FORCE_BUILD_MEDIA', '1')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djblets.settings')
 
-    if hasattr(django, 'setup'):
-        # Django >= 1.7
-        django.setup()
+    django.setup()
 
     # This will raise a CommandError or call sys.exit(1) on failure.
     call_command('collectstatic', interactive=False, verbosity=2)
