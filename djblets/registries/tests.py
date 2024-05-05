3b6426e983645f506f496b5ef6aff876b5fb9d18
Christian Hammond <christian@beanbaginc.com>
2024-05-04 18:29:30 -0700

    Make registries thread-safe, and add pre/post hooks for operations.
    We've had some long-standing problems where, on occasion, two threads
    will attempt to populate or manipulate a registry at the same time and
    end up with bad results. The common scenario is that two threads would
    initiate population of the registry, one would win, and the other would
    see 0 items in the registry.
    
    Registries were not built to be thread-safe, but they should have been.
    Locks around mutation operations can do a lot to help with that, amd
    this change introduces those.
    
    Where this got more complicated is how we handled state management and
    registry customization.
    
    Registries were previously in one of two states: Populated, or
    unpopulated. When population started, but before any default items were
    registered, the registry was put into the Populated state, which would
    make any other threads think that it was ready to pull items out of the
    registry.
    
    What we needed was a third state: Populating. This is set when
    population begins, but before/during default item registration. To
    represent this, we now have a `RegistryState` enum, which represents a
    `PENDING` (initial) state, `POPULATING`, and `READY` (populated).
    
    The old `populating` property now returns `True` if this state is
    anything but `PENDING`, helping cover existing behavior. This is
    deprecated in favor of `state`.
    
    The other part is registry customization. We had registries that would
    subclass `populate()`, `register()`, and `unregister()`, adding
    specialized pre/post behavior. However, this wasn't compatible with how
    locks and state are now managed. The solution here is to have new hook
    methods that subclasses can override to manage state at the right time
    and with thread-safety guarantees.
    
    Existing registries will continue to work, but just won't have the full
    degree of thread safety until they move to the hooks.
    
    The same reentrant lock is shared for all mutation operations, which
    helps ensure that one thread won't be populating while another is
    resetting or registering. It's still possible that one thread may expect
    an item to be there that won't exist due to another thread's operations,
    but now at least there won't be mixed state, and it's still always up to
    the caller to handle any exceptions anyway.
    
    Along with this, thread safety has been added to
    `lazy_import_registry()`. Previously, if two threads called this
    simultaneously, multiple registries could be created, though only one
    would be assigned to the resulting variable. Now there's a lock in
    place, ensuring only one is ever created. This was likely never an issue
    in practice, but it's worth ensuring we don't have side effects from
    redundant registry creation.
    
    Testing Done:
    Djblets and Review Board unit tests pass.
    
    Ran each of these Djblets Registry threading tests without the locks.
    It reproduced the issues we've seen sporadically in production.
    
    This will need real-world testing in production.
    
    Reviewed at https://reviews.reviewboard.org/r/13802/

--- djblets/registries/tests.py
+++ djblets/registries/tests.py
@@ -1,4 +1,10 @@
-from typing import Any, Iterable
+"""Unit tests for djblets.registries."""
+
+from __future__ import annotations
+
+import time
+from threading import Lock, Thread
+from typing import Any, Callable, Iterable
 
 from kgb import SpyAgency
 
@@ -56,6 +62,47 @@ class Item:
                     for attr_name in self._attrs))
 
 
+class ThreadRegistry(Registry[Item]):
+    """A registry used for thread tests.
+
+    Version Added:
+        5.0
+    """
+
+    lookup_attrs = ('id',)
+
+    ######################
+    # Instance variables #
+    ######################
+
+    count: int
+    sleep_lock: Lock
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialize the registry.
+
+        Args:
+            *args (tuple):
+                Positional arguments to pass to the parent.
+
+            **kwargs (tuple):
+                Keyword arguments to pass to the parent.
+        """
+        super().__init__(*args, **kwargs)
+
+        self.count = 0
+        self.sleep_lock = Lock()
+
+    def sleep_first_thread(self) -> None:
+        """Sleep the first time a thread calls this method."""
+        with self.sleep_lock:
+            do_sleep = (self.count == 0)
+            self.count += 1
+
+        if do_sleep:
+            time.sleep(0.1)
+
+
 class RegistryTests(SpyAgency, TestCase):
     """Tests for djblets.registries.Registry."""
 
@@ -73,9 +120,13 @@ class RegistryTests(SpyAgency, TestCase):
         with self.assertRaises(ItemLookupError):
             r.get('foo', 'bar')
 
-    def test_register_item(self):
+    def test_register_item(self) -> None:
         """Testing Registry.register_item"""
         r = Registry()
+
+        self.spy_on(r.on_item_registering)
+        self.spy_on(r.on_item_registered)
+
         items = [1, 2, 3]
 
         for item in items:
@@ -83,10 +134,68 @@ class RegistryTests(SpyAgency, TestCase):
 
         self.assertEqual(set(r), set(items))
 
+        self.assertSpyCallCount(r.on_item_registering, 3)
+        self.assertSpyCalledWith(r.on_item_registering, 1)
+        self.assertSpyCalledWith(r.on_item_registering, 2)
+        self.assertSpyCalledWith(r.on_item_registering, 3)
+
+        self.assertSpyCallCount(r.on_item_registered, 3)
+        self.assertSpyCalledWith(r.on_item_registered, 1)
+        self.assertSpyCalledWith(r.on_item_registered, 2)
+        self.assertSpyCalledWith(r.on_item_registered, 3)
+
+    def test_register_item_with_thread_conflict(self) -> None:
+        """Testing Registry.register_item with same item in different threads
+        """
+        def _thread_main(
+            secs: float,
+            item: Item,
+            expect_fail: bool,
+        ) -> None:
+            time.sleep(secs)
+
+            if expect_fail:
+                with self.assertRaises(AlreadyRegisteredError):
+                    registry.register(item)
+            else:
+                registry.register(item)
+
+        class TestRegistry(ThreadRegistry):
+            def on_item_registering(
+                self,
+                item: Item,
+            ) -> None:
+                self.sleep_first_thread()
+
+        registry = TestRegistry()
+        registry.populate()
+        self.spy_on(registry.on_item_registering)
+        self.spy_on(registry.on_item_registered)
+
+        self.assertEqual(len(registry), 0)
+
+        item1 = Item(id=0)
+        item2 = Item(id=0)
+
+        self._run_threads(
+            target=_thread_main,
+            threads_args=[
+                [0.1, item1, True],
+                [0.05, item2, False],
+            ])
+
+        self.assertIs(registry.get('id', 0), item2)
+
+        self.assertSpyCallCount(registry.on_item_registering, 2)
+        self.assertSpyCallCount(registry.on_item_registered, 1)
+
     def test_unregister_item(self):
         """Testing Registry.unregister_item"""
         r = Registry()
         items = [1, 2, 3]
+
+        self.spy_on(r.on_item_unregistering)
+        self.spy_on(r.on_item_unregistered)
 
         for item in items:
             r.register(item)
@@ -95,6 +204,59 @@ class RegistryTests(SpyAgency, TestCase):
             r.unregister(item)
 
         self.assertEqual(set(r), set())
+
+        self.assertSpyCallCount(r.on_item_unregistering, 3)
+        self.assertSpyCalledWith(r.on_item_unregistering, 1)
+        self.assertSpyCalledWith(r.on_item_unregistering, 2)
+        self.assertSpyCalledWith(r.on_item_unregistering, 3)
+
+        self.assertSpyCallCount(r.on_item_unregistered, 3)
+        self.assertSpyCalledWith(r.on_item_unregistered, 1)
+        self.assertSpyCalledWith(r.on_item_unregistered, 2)
+        self.assertSpyCalledWith(r.on_item_unregistered, 3)
+
+    def test_unregister_item_with_thread_conflict(self) -> None:
+        """Testing Registry.unregister_item with same item in different
+        threads
+        """
+        def _thread_main(
+            secs: float,
+            expect_fail: bool,
+        ) -> None:
+            time.sleep(secs)
+
+            if expect_fail:
+                with self.assertRaises(ItemLookupError):
+                    registry.unregister(item1)
+            else:
+                registry.unregister(item1)
+
+        class TestRegistry(ThreadRegistry):
+            def on_item_unregistering(
+                self,
+                item: Item,
+            ) -> None:
+                self.sleep_first_thread()
+
+        registry = TestRegistry()
+        self.spy_on(registry.on_item_unregistering)
+        self.spy_on(registry.on_item_unregistered)
+
+        item1 = Item(id=0)
+        registry.register(item1)
+
+        self.assertEqual(len(registry), 1)
+
+        self._run_threads(
+            target=_thread_main,
+            threads_args=[
+                [0.1, True],
+                [0.05, False],
+            ])
+
+        self.assertEqual(len(registry), 0)
+        self.assertSpyCallCount(registry.on_item_unregistering, 2)
+        self.assertSpyCallCount(registry.on_item_unregistered, 1)
 
     def test_unregister_removes_attr_lookups(self):
         """Testing Registry.unregister removes lookup entries"""
@@ -114,6 +276,80 @@ class RegistryTests(SpyAgency, TestCase):
                 r.get('id', item.id)
 
         self.assertEqual(len(r), 0)
+
+    def test_populate(self) -> None:
+        """Testing Registry.populate"""
+        item1 = Item(id=0)
+        item2 = Item(id=1)
+
+        class TestRegistry(Registry[Item]):
+            lookup_attrs = ('id',)
+
+            def get_defaults(self):
+                yield item1
+                yield item2
+
+        self.spy_on(registry_populating.send)
+
+        r = TestRegistry()
+        self.spy_on(r.on_populated)
+        self.spy_on(r.on_populating)
+
+        r.populate()
+
+        self.assertEqual(r._registry, {
+            'id': {
+                0: item1,
+                1: item2,
+            },
+        })
+
+        self.assertSpyCalled(r.on_populated)
+        self.assertSpyCalled(r.on_populating)
+
+        self.assertIs(r.get('id', item1.id), item1)
+        self.assertIs(r.get('id', item2.id), item2)
+
+        self.assertSpyCalledWith(
+            registry_populating.send,
+            sender=TestRegistry,
+            registry=r)
+
+    def test_populate_with_thread_conflict(self) -> None:
+        """Testing Registry.populate with multiple threads"""
+        def _thread_main(
+            secs: float,
+        ) -> None:
+            time.sleep(secs)
+            registry.populate()
+
+            self.assertEqual(list(registry._items), [item1, item2])
+
+        class TestRegistry(ThreadRegistry):
+            def get_defaults(self):
+                yield item1
+                yield item2
+
+            def on_populating(self) -> None:
+                self.sleep_first_thread()
+
+        registry = TestRegistry()
+        self.spy_on(registry.on_populating)
+        self.spy_on(registry.on_populated)
+
+        item1 = Item(id=0)
+        item2 = Item(id=1)
+
+        self._run_threads(
+            target=_thread_main,
+            threads_args=[
+                [0.1],
+                [0.05],
+            ])
+
+        self.assertEqual(list(registry._items), [item1, item2])
+        self.assertSpyCallCount(registry.on_populating, 1)
+        self.assertSpyCallCount(registry.on_populated, 1)
 
     def test_population_on_register(self):
         """Testing Registry.register_item triggers population before
@@ -198,6 +434,73 @@ class RegistryTests(SpyAgency, TestCase):
         with self.assertRaises(RegistrationError):
             r.register(Item())
 
+    def test_reset(self) -> None:
+        """Testing Registry.reset"""
+        item1 = Item(id=0)
+        item2 = Item(id=1)
+
+        class TestRegistry(Registry[Item]):
+            lookup_attrs = ('id',)
+
+            def get_defaults(self):
+                yield item1
+                yield item2
+
+        self.spy_on(registry_populating.send)
+
+        r = TestRegistry()
+        self.spy_on(r.on_resetting)
+        self.spy_on(r.on_reset)
+
+        r.populate()
+        r.reset()
+
+        self.assertEqual(r._registry, {
+            'id': {},
+        })
+
+        self.assertSpyCalled(r.on_resetting)
+        self.assertSpyCalled(r.on_reset)
+
+    def test_reset_with_thread_conflict(self) -> None:
+        """Testing Registry.reset with multiple threads"""
+        def _thread_main(
+            secs: float,
+        ) -> None:
+            self.assertEqual(list(registry._items), [item1, item2])
+
+            time.sleep(secs)
+            registry.reset()
+
+            self.assertEqual(list(registry._items), [])
+
+        class TestRegistry(ThreadRegistry):
+            def get_defaults(self):
+                yield item1
+                yield item2
+
+            def on_resetting(self) -> None:
+                self.sleep_first_thread()
+
+        item1 = Item(id=0)
+        item2 = Item(id=1)
+
+        registry = TestRegistry()
+        registry.populate()
+        self.spy_on(registry.on_resetting)
+        self.spy_on(registry.on_reset)
+
+        self._run_threads(
+            target=_thread_main,
+            threads_args=[
+                [0.1],
+                [0.05],
+            ])
+
+        self.assertEqual(len(registry._items), 0)
+        self.assertSpyCallCount(registry.on_resetting, 1)
+        self.assertSpyCallCount(registry.on_reset, 1)
+
     def test_contains(self):
         """Testing Registry.__contains__"""
         r = Registry()
@@ -218,6 +521,42 @@ class RegistryTests(SpyAgency, TestCase):
         with self.assertRaisesMessage(ItemLookupError,
                                       'The foo "1" is unregistered.'):
             r.unregister(1)
+
+    def _run_threads(
+        self,
+        target: Callable[..., None],
+        threads_args: list[list[Any]],
+    ) -> None:
+        """Run tests with a specified number of threads.
+
+        This will create multiple threads with the same target and different
+        arguments, starting them up and then joining them back to the main
+        thread.
+
+        Version Added:
+            5.0
+
+        Args:
+            target (callable):
+                The function for each thread to call when started.
+
+            threads_args (list):
+                Arguments to provide for each created thread.
+
+                The number of items in this list dictates the number of
+                threads.
+        """
+        threads = [
+            Thread(target=target,
+                   args=thread_args)
+            for thread_args in threads_args
+        ]
+
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
 
 
 class OrderedRegistryTests(TestCase):
