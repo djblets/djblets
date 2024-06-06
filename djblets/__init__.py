83b708642d343402d531bde60547b5c9f9e15352
Christian Hammond <christian@beanbaginc.com>
2024-06-06 01:47:41 -0700

    Bump to 5.0.1 dev.

--- djblets/__init__.py
+++ djblets/__init__.py
@@ -6,7 +6,7 @@
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (5, 0, 0, 'final', 0, True)
+VERSION = (5, 0, 1, 'alpha', 0, False)
 
 
 def get_version_string():
