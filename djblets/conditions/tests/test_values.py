d317571ae11d9067b19fdf0b557c3bd1e7e8c5af
Christian Hammond <christian@beanbaginc.com>
2025-02-13 18:03:35 -0800

    Add typing for condition values.
    This updates `djblets.conditions.values` to support typing.
    `BaseConditionValueField` and `ConditionValueFormField` are now
    generics, accepting a type that the fields manage. These types are
    accepted in `serialize_value()` and returned in `deserialize_value()`.
    Other value subclasses (for model values) also take a type.
    
    If a type is not provided, these default to `Any`. This is available as
    part of PEP 696 and a compatible ``typing_extensions``.
    
    Some Python 2.7-safe code has been removed, cleaning up some tests and
    implementation code.
    
    This is the first step in making condition support type-safe, and
    modernizing some of the APIs for better type compliance.
    
    Testing Done:
    Unit tests pass.
    
    Reviewed at https://reviews.reviewboard.org/r/14264/

--- djblets/conditions/tests/test_values.py
+++ djblets/conditions/tests/test_values.py
@@ -1,9 +1,15 @@
+"""Unit tests for djblets.conditions.values."""
+
+from __future__ import annotations
+
 import re
+from typing import Any, Optional, Sequence
 
 from django import forms
 from django.contrib.sites.models import Site
 from django.db.models.query import QuerySet
 from django.http import QueryDict
+from typing_extensions import assert_type
 
 from djblets.conditions.errors import InvalidConditionValueError
 from djblets.conditions.values import (ConditionValueBooleanField,
@@ -18,156 +24,216 @@ from djblets.testing.testcases import TestCase
 class ConditionValueFormFieldTests(TestCase):
     """Unit tests for djblets.conditions.values.ConditionValueFormField."""
 
-    def test_serialize_value(self):
+    def test_serialize_value(self) -> None:
         """Testing ConditionValueFormField.serialize_value"""
         # ModelChoiceField uses prepare_value(), needed by serialize_value(),
         # so it's a good test.
         field = ConditionValueFormField(
             forms.ModelChoiceField(queryset=Site.objects.all()))
+        assert_type(field, ConditionValueFormField[Any])
 
         site = Site.objects.get_current()
         self.assertEqual(field.serialize_value(site), site.pk)
 
-    def test_deserialize_value(self):
+    def test_deserialize_value(self) -> None:
         """Testing ConditionValueFormField.deserialize_value"""
         field = ConditionValueFormField(
             forms.ModelChoiceField(queryset=Site.objects.all()))
+        assert_type(field, ConditionValueFormField[Any])
 
         site = Site.objects.get_current()
         self.assertEqual(field.deserialize_value(site.pk), site)
 
-    def test_get_from_form_data(self):
+    def test_get_from_form_data(self) -> None:
         """Testing ConditionValueFormField.get_from_form_data"""
-        field = ConditionValueFormField(forms.CharField())
+        field = ConditionValueFormField[str](forms.CharField())
 
         data = QueryDict('test-field=abc123')
         files = QueryDict('')
 
-        self.assertEqual(field.get_from_form_data(data, files, 'test-field'),
-                         'abc123')
+        value = field.get_from_form_data(data, files, 'test-field')
 
-    def test_render_html(self):
+        assert_type(value, Optional[str])
+        self.assertEqual(value, 'abc123')
+
+    def test_render_html(self) -> None:
         """Testing ConditionValueFormField.render_html"""
-        field = ConditionValueFormField(forms.CharField())
+        field = ConditionValueFormField[str](forms.CharField())
 
         self.assertHTMLEqual(field.render_html(),
                              '<input name="XXX" type="text" />')
 
-    def test_field_with_callable(self):
+    def test_field_with_callable(self) -> None:
         """Testing ConditionValueFormField.field with callable field"""
-        field = ConditionValueFormField(lambda: forms.CharField())
+        field = ConditionValueFormField[str](lambda: forms.CharField())
 
-        self.assertTrue(isinstance(field.field, forms.CharField))
+        self.assertIsInstance(field.field, forms.CharField)
 
 
 class ConditionValueBooleanFieldTests(TestCase):
     """Unit tests for djblets.conditions.values.ConditionValueBooleanField."""
 
-    def setUp(self):
-        super(ConditionValueBooleanFieldTests, self).setUp()
-
-        self.value_field = ConditionValueBooleanField()
-
-    def test_deserialize_value_with_true(self):
+    def test_deserialize_value_with_true(self) -> None:
         """Testing ConditionValueBooleanField.deserialize_value with True"""
-        self.assertTrue(self.value_field.deserialize_value(True))
-        self.assertTrue(self.value_field.deserialize_value('true'))
-        self.assertTrue(self.value_field.deserialize_value('True'))
-        self.assertTrue(self.value_field.deserialize_value('1'))
+        value_field = ConditionValueBooleanField()
 
-    def test_deserialize_value_with_false(self):
+        value = value_field.deserialize_value(True)
+        assert_type(value, bool)
+        self.assertTrue(value)
+
+        value = value_field.deserialize_value('true')
+        assert_type(value, bool)
+        self.assertTrue(value)
+
+        value = value_field.deserialize_value('True')
+        assert_type(value, bool)
+        self.assertTrue(value)
+
+        value = value_field.deserialize_value('1')
+        assert_type(value, bool)
+        self.assertTrue(value)
+
+    def test_deserialize_value_with_false(self) -> None:
         """Testing ConditionValueBooleanField.deserialize_value with False"""
-        self.assertFalse(self.value_field.deserialize_value(False))
-        self.assertFalse(self.value_field.deserialize_value('false'))
-        self.assertFalse(self.value_field.deserialize_value('False'))
-        self.assertFalse(self.value_field.deserialize_value('0'))
+        value_field = ConditionValueBooleanField()
 
-    def test_prepare_value_for_widget(self):
+        value = value_field.deserialize_value(False)
+        assert_type(value, bool)
+        self.assertFalse(value)
+
+        value = value_field.deserialize_value('false')
+        assert_type(value, bool)
+        self.assertFalse(value)
+
+        value = value_field.deserialize_value('False')
+        assert_type(value, bool)
+        self.assertFalse(value)
+
+        value = value_field.deserialize_value('0')
+        assert_type(value, bool)
+        self.assertFalse(value)
+
+    def test_serialize_value(self) -> None:
+        """Testing ConditionValueBooleanField.serialize_value"""
+        value_field = ConditionValueBooleanField()
+
+        self.assertIs(value_field.serialize_value(True), True)
+        self.assertIs(value_field.serialize_value(False), False)
+
+    def test_prepare_value_for_widget(self) -> None:
         """Testing ConditionValueBooleanField.prepare_value_for_widget"""
-        self.assertEqual(self.value_field.prepare_value_for_widget(True),
-                         'True')
-        self.assertEqual(self.value_field.prepare_value_for_widget(False),
-                         'False')
+        value_field = ConditionValueBooleanField()
+
+        self.assertEqual(value_field.prepare_value_for_widget(True), 'True')
+        self.assertEqual(value_field.prepare_value_for_widget(False), 'False')
 
 
 class ConditionValueIntegerFieldTests(TestCase):
     """Unit tests for djblets.conditions.values.ConditionValueIntegerField."""
 
-    def setUp(self):
-        super(ConditionValueIntegerFieldTests, self).setUp()
-
-        self.value_field = ConditionValueIntegerField()
-
-    def test_deserialize_value(self):
+    def test_deserialize_value(self) -> None:
         """Testing ConditionValueIntegerField.deserialize_value"""
-        self.assertEqual(self.value_field.deserialize_value(100), 100)
-        self.assertEqual(self.value_field.deserialize_value('100'), 100)
+        value_field = ConditionValueIntegerField()
+
+        value = value_field.deserialize_value(100)
+        assert_type(value, int)
+        self.assertEqual(value, 100)
+
+        value = value_field.deserialize_value('100')
+        assert_type(value, int)
+        self.assertEqual(value, 100)
+
+    def test_serialize_value(self) -> None:
+        """Testing ConditionValueIntegerField.serialize_value"""
+        value_field = ConditionValueIntegerField()
+
+        self.assertEqual(value_field.serialize_value(100), 100)
 
 
 class ConditionValueModelFieldTests(TestCase):
     """Unit tests for ConditionValueModelField."""
 
-    def test_init_with_queryset(self):
+    def test_init_with_queryset(self) -> None:
         """Testing ConditionValueModelField initialization with QuerySet"""
-        field = ConditionValueModelField(queryset=Site.objects.all())
+        field = ConditionValueModelField[Site](queryset=Site.objects.all())
 
-        self.assertTrue(isinstance(field.field.queryset, QuerySet))
+        form_field = field.field
+        assert isinstance(form_field, forms.ModelChoiceField)
 
-    def test_init_with_callable(self):
+        self.assertIsInstance(form_field.queryset, QuerySet)
+
+    def test_init_with_callable(self) -> None:
         """Testing ConditionValueModelField initialization with callable"""
-        field = ConditionValueModelField(queryset=lambda: Site.objects.all())
+        field = ConditionValueModelField[Site](
+            queryset=lambda: Site.objects.all())
 
-        self.assertTrue(isinstance(field.field.queryset, QuerySet))
+        form_field = field.field
+        assert isinstance(form_field, forms.ModelChoiceField)
 
-    def test_serialize_value(self):
+        self.assertIsInstance(form_field.queryset, QuerySet)
+
+    def test_serialize_value(self) -> None:
         """Testing ConditionValueModelField.serialize_value"""
-        field = ConditionValueModelField(queryset=Site.objects.all())
+        field = ConditionValueModelField[Site](queryset=Site.objects.all())
 
         site = Site.objects.get_current()
         self.assertEqual(field.serialize_value(site), site.pk)
 
-    def test_deserialize_value(self):
+    def test_deserialize_value(self) -> None:
         """Testing ConditionValueModelField.deserialize_value"""
-        field = ConditionValueModelField(queryset=Site.objects.all())
-
+        field = ConditionValueModelField[Site](queryset=Site.objects.all())
         site = Site.objects.get_current()
-        self.assertEqual(field.deserialize_value(site.pk), site)
+
+        value = field.deserialize_value(site.pk)
+        assert_type(value, Site)
+
+        self.assertEqual(value, site)
 
 
 class ConditionValueMultipleModelFieldTests(TestCase):
     """Unit tests for ConditionValueMultipleModelField."""
 
-    def test_init_with_queryset(self):
+    def test_init_with_queryset(self) -> None:
         """Testing ConditionValueMultipleModelField initialization with
         QuerySet
         """
-        field = ConditionValueMultipleModelField(queryset=Site.objects.all())
+        field = ConditionValueMultipleModelField[Site](
+            queryset=Site.objects.all())
 
-        self.assertTrue(isinstance(field.field.queryset, QuerySet))
+        form_field = field.field
+        assert isinstance(form_field, forms.ModelMultipleChoiceField)
 
-    def test_init_with_callable(self):
+        self.assertIsInstance(form_field.queryset, QuerySet)
+
+    def test_init_with_callable(self) -> None:
         """Testing ConditionValueMultipleModelField initialization with
         callable
         """
-        field = ConditionValueMultipleModelField(
+        field = ConditionValueMultipleModelField[Site](
             queryset=lambda: Site.objects.all())
 
-        self.assertTrue(isinstance(field.field.queryset, QuerySet))
+        form_field = field.field
+        assert isinstance(form_field, forms.ModelMultipleChoiceField)
 
-    def test_serialize_value(self):
+        self.assertIsInstance(form_field.queryset, QuerySet)
+
+    def test_serialize_value(self) -> None:
         """Testing ConditionValueMultipleModelField.serialize_value"""
-        field = ConditionValueMultipleModelField(queryset=Site.objects.all())
+        field = ConditionValueMultipleModelField[Site](
+            queryset=Site.objects.all())
 
         site = Site.objects.get_current()
         self.assertEqual(field.serialize_value([site]), [site.pk])
 
-    def test_deserialize_value(self):
+    def test_deserialize_value(self) -> None:
         """Testing ConditionValueMultipleModelField.deserialize_value"""
-        field = ConditionValueMultipleModelField(queryset=Site.objects.all())
+        field = ConditionValueMultipleModelField[Site](
+            queryset=Site.objects.all())
 
         site = Site.objects.get_current()
         result = field.deserialize_value([site.pk])
 
+        assert_type(result, Sequence[Site])
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0], site)
 
@@ -175,33 +241,27 @@ class ConditionValueMultipleModelFieldTests(TestCase):
 class ConditionValueRegexFieldTests(TestCase):
     """Unit tests for djblets.conditions.values.ConditionValueRegexField."""
 
-    def setUp(self):
+    def setUp(self) -> None:
         super(ConditionValueRegexFieldTests, self).setUp()
 
         self.value_field = ConditionValueRegexField()
 
-    def test_deserialize_value(self):
+    def test_deserialize_value(self) -> None:
         """Testing ConditionValueRegexField.deserialize_value"""
         regex_obj = self.value_field.deserialize_value('ab[^c]d+e?')
 
-        # Weird, right? re.RegexObject, the documented result of a
-        # re.compile(), does not exist. Instead, it's a _sre.SRE_Pattern,
-        # but that's basically an internal detail we don't want to rely upon.
-        # Internally, the re module actually does this type() mess to get
-        # the type to use, so that's what we're doing!
-        regex_type = type(re.compile(''))
-
-        self.assertTrue(isinstance(regex_obj, regex_type))
+        assert_type(regex_obj, re.Pattern)
+        self.assertIsInstance(regex_obj, re.Pattern)
         self.assertEqual(regex_obj.pattern, re.compile('ab[^c]d+e?').pattern)
 
-    def test_deserialize_value_with_bad_pattern(self):
+    def test_deserialize_value_with_bad_pattern(self) -> None:
         """Testing ConditionValueRegexField.deserialize_value with bad
         pattern
         """
         with self.assertRaises(InvalidConditionValueError):
             self.value_field.deserialize_value('*')
 
-    def test_serialize_value(self):
+    def test_serialize_value(self) -> None:
         """Testing ConditionValueRegexField.serialize_value"""
         regex_obj = re.compile('ab[^c]d+e?')
 
