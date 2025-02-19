aa6d3168f97ac6ec6f2081972223df7acaebf780
Christian Hammond <christian@beanbaginc.com>
2025-02-19 12:33:47 -0800

    Update Ink to 0.7.
    Ink 0.7 introduces a new Help icon, new units, and includes several
    layout and presentation bug fixes.
    
    Testing Done:
    Built the static media in Djblets and Review Board. Verified that the
    new icon and units were available and worked as expected.
    
    Reviewed at https://reviews.reviewboard.org/r/14353/

--- djblets/dependencies.py
+++ djblets/dependencies.py
@@ -83,7 +83,7 @@ package_dependencies = {
 #: Dependencies required for static media building.
 frontend_buildkit_npm_dependencies: Dict[str, str] = {
     '@beanbag/frontend-buildkit': '^1.2.0',
-    '@beanbag/ink': '^0.6.0',
+    '@beanbag/ink': '^0.7.0',
     '@beanbag/spina': '^3.1.1',
     '@types/jquery': '^3.5.30',
     '@types/underscore': '^1.11.4',
