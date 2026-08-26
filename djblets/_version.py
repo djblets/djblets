62460046337ec1cb1970c89e7d9de2fe69b94d6e
Christian Hammond <christian@beanbaginc.com>
2026-08-25 21:31:32 -0700

    Bump to 6.1.1 dev.

--- djblets/_version.py
+++ djblets/_version.py
@@ -25,7 +25,7 @@ if TYPE_CHECKING:
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION: _Version = (6, 1, 0, 'final', 0, True)
+VERSION: _Version = (6, 1, 1, 'alpha', 0, False)
 
 
 def get_version_string() -> str:
